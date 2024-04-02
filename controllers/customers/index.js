1e00c5e550ad5ada2e90b83f8b133d0f6eeaf203
Amin Babu <amin.babu.bd@gmail.com>
2024-04-02 21:52:30 +0600

    Update date format in customer controller

--- controllers/customers/index.js
+++ controllers/customers/index.js
@@ -3,8 +3,8 @@
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 29 March 2024
- * @update_date 29 March 2024
+ * @date 29 March, 2024
+ * @update_date 29 March, 2024
  */
 
 // export all controllers
@@ -12,6 +12,5 @@ module.exports = {
     getAllCustomers: require('./get-all-customers'),
     getCustomerById: require('./get-customer-by-id'),
     updateCustomerById: require('./update-customer-by-id'),
-    updateWalletByCustomerId: require('./update-wallet-by-customer-id'),
     deleteCustomerById: require('./delete-customer-by-id'),
 };
