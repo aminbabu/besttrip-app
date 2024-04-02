3069e3f4366f79c651816ee584adc80aa88bbf05
Amin Babu <amin.babu.bd@gmail.com>
2024-04-03 04:47:17 +0600

    Remove wallet population from getAllCustomers

--- controllers/customers/get-all-customers.js
+++ controllers/customers/get-all-customers.js
@@ -15,7 +15,7 @@ const { generateToken } = require('../../utils');
 const getAllCustomers = async (req, res, next) => {
     try {
         // get all customers
-        const customers = await Customer.find().populate('wallet');
+        const customers = await Customer.find();
 
         // generate token
         const token = generateToken(req.user);
