52dd096be4f5b4bdd8bada67fcd8fdd978444f61
Amin Babu <amin.babu.bd@gmail.com>
2024-04-03 04:50:47 +0600

    Add customer update validator

---
+++ middlewares/validators/customers/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /middlewares/validators/customers/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 03 April, 2024
+ * @update_date 03 April, 2024
+ */
+
+// export all validators
+module.exports = {
+    updateCustomer: require('./update-customer'),
+};
