0b12a43b9fe58c9650f9a816b39ad3602c09af44
Amin Babu <amin.babu.bd@gmail.com>
2024-04-03 05:08:24 +0600

    Update customer validator with optional fields

--- middlewares/validators/customers/update-customer.js
+++ middlewares/validators/customers/update-customer.js
@@ -9,71 +9,34 @@
 
 // dependencies
 const { body } = require('express-validator');
+const { expressValidator } = require('../../../handlers/errors');
 
 // update customer validator
 module.exports = [
     body('avatar').optional().isURL().withMessage('avatar should be a URL'),
-    body('name')
-        .exists()
-        .withMessage('name is required')
-        .isString()
-        .withMessage('name should be a string'),
-    body('email')
-        .exists()
-        .withMessage('email is required')
-        .isEmail()
-        .withMessage('email should be an email'),
-    body('phone')
-        .exists()
-        .withMessage('phone is required')
-        .isMobilePhone()
-        .withMessage('phone should be a phone number'),
+    body('name').optional().isString().withMessage('name should be a string'),
+    body('email').optional().isEmail().withMessage('email should be an email'),
+    body('phone').optional().isMobilePhone().withMessage('phone should be a phone number'),
     body('password')
-        .exists()
-        .withMessage('password is required')
+        .optional()
         .isLength({ min: 6 })
         .withMessage('password should be at least 6 characters'),
-    body('dob')
-        .exists()
-        .withMessage('dob is required')
-        .isISO8601()
-        .withMessage('dob should be a date'),
+    body('dob').optional().isISO8601().withMessage('dob should be a date'),
     body('status')
-        .exists()
-        .withMessage('status is required')
+        .optional()
         .isIn(['active', 'disabled'])
         .withMessage('status should be active or disabled'),
-    body('address')
-        .exists()
-        .withMessage('address is required')
-        .isString()
-        .withMessage('address should be a string'),
-    body('city')
-        .exists()
-        .withMessage('city is required')
-        .isString()
-        .withMessage('city should be a string'),
-    body('state')
-        .exists()
-        .withMessage('state is required')
-        .isString()
-        .withMessage('state should be a string'),
-    body('country')
-        .exists()
-        .withMessage('country is required')
-        .isString()
-        .withMessage('country should be a string'),
+    body('address').optional().isString().withMessage('address should be a string'),
+    body('city').optional().isString().withMessage('city should be a string'),
+    body('state').optional().isString().withMessage('state should be a string'),
+    body('country').optional().isString().withMessage('country should be a string'),
     body('postalCode')
-        .exists()
-        .withMessage('postalCode is required')
-        .isPostalCode()
+        .optional()
+        .isPostalCode('any')
         .withMessage('postalCode should be a postal code'),
-    body('flyerNumber')
-        .exists()
-        .withMessage('flyerNumber is required')
-        .isString()
-        .withMessage('flyerNumber should be a string'),
+    body('flyerNumber').optional().isString().withMessage('flyerNumber should be a string'),
     body('wallet')
+        .optional()
         .isObject()
         .withMessage('wallet should be an object')
         .custom((value) => {
@@ -85,4 +48,5 @@ module.exports = [
             }
             return true;
         }),
+    expressValidator,
 ];
