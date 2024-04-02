58563bd95e37166024f1841580f24783dd5046f0
Amin Babu <amin.babu.bd@gmail.com>
2024-04-02 19:53:42 +0600

    Fix isVerified function call in user login route

--- routes/auth/users/index.js
+++ routes/auth/users/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March 2024
- * @update_date 29 March 2024
+ * @update_date 29 March, 2024
  */
 
 // dependencies
@@ -39,7 +39,7 @@ router.post('/register', auth.validateRegister, user.register);
  * @access public
  * @method POST
  */
-router.post('/login', auth.validateLogin, isVerified, user.login);
+router.post('/login', auth.validateLogin, isVerified.user, user.login);
 
 /**
  * @description forgot password
