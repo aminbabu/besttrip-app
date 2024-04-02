cac9965cdbfb5fda03a7b6492685a9b3adc434ae
Amin Babu <amin.babu.bd@gmail.com>
2024-04-02 21:31:26 +0600

    Update customer routes with authorization and permission checks

--- routes/customers/index.js
+++ routes/customers/index.js
@@ -3,8 +3,8 @@
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 29 March 2024
- * @update_date 29 March 2024
+ * @date 29 March, 2024
+ * @update_date 29 March, 2024
  */
 
 // dependencies
@@ -22,51 +22,57 @@ const {
 
 // middlewares
 // const { customers } = require('../../middlewares/validators');
-const { isAuthorized } = require('../../middlewares/auth');
+const { isAuthorized, isAllowed } = require('../../middlewares/auth');
+
+/**
+ * @description check if user is authorized
+ * @param {string} path - /customers
+ * @param {function} middleware - ['isAuthorized', 'isAllowed']
+ * @returns {object} - router
+ * @access private
+ * @method USE
+ */
+router.use(isAuthorized, isAllowed(['admin']));
 
 /**
  * @description get all customers
- * @param {string} path - /customers/
- * @param {function} middleware - ['isAuthorized']
+ * @param {string} path - /customers
  * @param {function} controller - ['getAllCustomers']
  * @returns {object} - router
  * @access private
  * @method GET
  */
-router.get('/', isAuthorized, getAllCustomers);
+router.get('/', getAllCustomers);
 
 /**
  * @description get by mongo id
  * @param {string} path - /customers/:id
- * @param {function} middleware - ['isAuthorized']
  * @param {function} controller - ['getCustomerById']
  * @returns {object} - router
  * @access private
  * @method GET
  */
-router.get('/:id', isAuthorized, getCustomerById);
+router.get('/:id', getCustomerById);
 
 /**
  * @description update customer by mongo id
  * @param {string} path - /customers/:id
- * @param {function} middleware - ['isAuthorized']
  * @param {function} controller - ['updateCustomerById']
  * @returns {object} - router
  * @access private
  * @method PATCH
  */
-router.patch('/:id', isAuthorized, updateCustomerById);
+router.patch('/:id', updateCustomerById);
 
 /**
  * @description delete customer by mongo id
  * @param {string} path - /customers/:id
- * @param {function} middleware - ['isAuthorized']
  * @param {function} controller - ['deleteCustomerById']
  * @returns {object} - router
  * @access private
  * @method DELETE
  */
-router.delete('/:id', isAuthorized, deleteCustomerById);
+router.delete('/:id', deleteCustomerById);
 
 // export
 module.exports = router;
