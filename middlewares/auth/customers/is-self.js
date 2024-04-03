3a09f9c24143044380a3fbf7244b044f2e4826ff
Amin Babu <amin.babu.bd@gmail.com>
2024-04-03 07:20:12 +0600

    Refactor customer self-check logic

--- middlewares/auth/customers/is-self.js
+++ middlewares/auth/customers/is-self.js
@@ -20,7 +20,7 @@ module.exports = async (req, res, next) => {
         const customer = await Customer.findById(id);
 
         // check if customer is self
-        if (customer._id.toString() !== req.user._id.toString()) {
+        if (customer?._id.toString() !== req.user?._id.toString()) {
             return res.status(403).json({
                 message: 'Forbidden',
             });
@@ -29,6 +29,7 @@ module.exports = async (req, res, next) => {
         // next middleware
         return next();
     } catch (error) {
+        console.log(error);
         // error response
         return res.status(500).json({
             message: 'Internal Server Error',
