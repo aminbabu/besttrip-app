93987678544627cf015131a28a297a8539d78eb3
Amin Babu <amin.babu.bd@gmail.com>
2024-04-04 02:22:03 +0600

    Update all customers API endpoint

--- controllers/customers/update-all-customers.js
+++ controllers/customers/update-all-customers.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 April, 2024
- * @update_date 03 April, 2024
+ * @update_date 04 April, 2024
  */
 
 // dependencies
@@ -13,12 +13,23 @@ const { Customer } = require('../../models');
 // update all customers
 const updateAllCustomers = async (req, res, next) => {
     try {
+        // Update all customers
         const customers = await Customer.updateMany({}, req.body);
 
+        // Check if any documents were modified
+        if (customers.matchedCount > 0) {
+            // Fetch all customers after update
+            const updatedCustomers = await Customer.find();
+
+            // Return response with updated data
+            return res.status(200).json({
+                message: 'All customers updated successfully',
+                customers: updatedCustomers,
+            });
+        }
         return res.status(200).json({
-            success: true,
-            message: 'all customers updated successfully',
-            customers,
+            message: 'No customers were updated',
+            customers: [],
         });
     } catch (error) {
         return next(error);
