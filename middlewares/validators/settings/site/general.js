54e6cd845d0e5d4d71341a64e3e9930db00dd895
Amin Babu <amin.babu.bd@gmail.com>
2024-04-04 23:19:20 +0600

    Refactor site general settings validator

--- middlewares/validators/settings/site/general.js
+++ middlewares/validators/settings/site/general.js
@@ -13,30 +13,14 @@ const { expressValidator } = require('../../../../handlers/errors');
 
 // export general site settings validator
 module.exports = [
-    body('logo')
-        .isEmpty()
-        .withMessage('Logo is required')
-        .isString()
-        .withMessage('Logo must be a string'),
-    body('favicon')
-        .isEmpty()
-        .withMessage('Favicon is required')
-        .isString()
-        .withMessage('Favicon must be a string'),
+    body('logo').optional().isString().withMessage('Logo must be a string'),
+    body('favicon').optional().isString().withMessage('Favicon must be a string'),
     body('title')
-        .isLength({ min: 3 })
-        .withMessage('Title is required and must be at least 3 characters')
+        .notEmpty()
+        .withMessage('Title is required')
         .isString()
         .withMessage('Title must be a string'),
-    body('domain')
-        .isEmpty()
-        .withMessage('Domain is required')
-        .isString()
-        .withMessage('Domain must be a string'),
-    body('description')
-        .isEmpty()
-        .withMessage('Description is required')
-        .isString()
-        .withMessage('Description must be a string'),
+    body('domain').optional().isString().withMessage('Domain must be a string'),
+    body('description').optional().isString().withMessage('Description must be a string'),
     expressValidator,
 ];
