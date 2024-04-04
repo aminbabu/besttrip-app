26b937cdf4e0da6366e49da26af3ddf6ff7d3d04
Amin Babu <amin.babu.bd@gmail.com>
2024-04-04 19:58:17 +0600

    Add settings controller for general settings

---
+++ controllers/settings/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /controllers/settings/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 04 April, 2024
+ * @update_date 04 April, 2024
+ */
+
+// export all settings controllers
+module.exports = {
+    getGeneralSettings: require('./site/general/getGeneralSettings'),
+};
