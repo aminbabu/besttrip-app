255dc1a1a8a796c4ca53880789523e3039fd9707
Amin Babu <amin.babu.bd@gmail.com>
2024-04-04 19:57:49 +0600

    Add getGeneralSettings controller

---
+++ controllers/settings/site/general/getGeneralSettings.js
@@ -0,0 +1,21 @@
+/**
+ * @file /controllers/settings/site/general/getGeneralSettings.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 04 April, 2024
+ * @update_date 04 April, 2024
+ */
+
+// dependencies
+const { GeneralSettings } = require('../../../../models/settings');
+
+// get general site settings controller
+module.exports = async (req, res, next) => {
+    try {
+        const generalSettings = await GeneralSettings.findOne();
+        return res.status(200).json(generalSettings);
+    } catch (error) {
+        return next(error);
+    }
+};
