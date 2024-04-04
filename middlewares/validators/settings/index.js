16ab361b79ef545f973bf871e9a512d00c81dde8
Amin Babu <amin.babu.bd@gmail.com>
2024-04-04 20:06:31 +0600

    Add settings validators middleware

---
+++ middlewares/validators/settings/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /middlewares/validators/settings/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 04 April, 2024
+ * @update_date 04 April, 2024
+ */
+
+// export all settings validators
+module.exports = {
+    general: require('./site/general'),
+};
