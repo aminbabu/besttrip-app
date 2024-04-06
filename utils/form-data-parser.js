afa07938603f2c046d4be0a27863d1a1d88c5dc1
Amin Babu <amin.babu.bd@gmail.com>
2024-04-06 17:23:25 +0600

    Refactor form-data-parser.js to merge fields and files into req.body

--- utils/form-data-parser.js
+++ utils/form-data-parser.js
@@ -20,8 +20,7 @@ module.exports = async (req, res, next) => {
         const [fields, files] = await form.parse(req);
 
         // set form data to request
-        req.body = fields;
-        req.files = files;
+        req.body = { ...req.body, ...fields, files };
 
         // next middleware
         return next();
