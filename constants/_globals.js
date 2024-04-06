ba237a99f4328bcd1c79505ddf0febf42e7adb3e
Amin Babu <amin.babu.bd@gmail.com>
2024-04-06 17:33:43 +0600

    Add _globals.js for defining global constants

---
+++ constants/_globals.js
@@ -0,0 +1,28 @@
+/**
+ * @file constants/_globals.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 06 April, 2024
+ * @update_date 06 April, 2024
+ */
+
+// pagination constants
+const DEFAULT_PAGE_LIMIT = 10;
+const DEFAULT_PAGE_NUMBER = 1;
+
+// file size constants
+const ONE_MEGA_BYTE = 1024 * 1024;
+const MIN_FILE_SIZE = ONE_MEGA_BYTE;
+const MAX_FILE_SIZE = 10 * ONE_MEGA_BYTE;
+const DEFAULT_FILE_SIZE = 5 * ONE_MEGA_BYTE;
+
+// export constants
+module.exports = {
+    ONE_MEGA_BYTE,
+    DEFAULT_PAGE_LIMIT,
+    DEFAULT_PAGE_NUMBER,
+    MIN_FILE_SIZE,
+    MAX_FILE_SIZE,
+    DEFAULT_FILE_SIZE,
+};
