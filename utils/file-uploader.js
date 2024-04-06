707eacab622cf946666e1ea6c921f2dd18886f5b
Amin Babu <amin.babu.bd@gmail.com>
2024-04-06 20:51:35 +0600

    Refactor file-uploader.js to use fs/promises for file operations and improve error handling

--- utils/file-uploader.js
+++ utils/file-uploader.js
@@ -8,41 +8,50 @@
  */
 
 // dependencies
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 // export file uploader
 module.exports = async (file) => {
-    // check if file is empty
-    if (!file) {
+    try {
+        // check if file is empty
+        if (!file) {
+            return null;
+        }
+
+        // get old path
+        const oldPath = file.filepath;
+
+        // define new path
+        const newPath = path.join(__dirname, '../public/uploads/logos/', file.originalFilename);
+
+        console.log(newPath, 'newPath');
+
+        // check if file exists
+        const exists = await fs
+            .access(newPath)
+            .then(() => true)
+            .catch(() => false);
+
+        // remove old file
+        if (exists) {
+            await fs.unlink(newPath);
+        }
+
+        // copy file to new path
+        await fs.copyFile(oldPath, newPath);
+
+        // remove old file
+        await fs.unlink(oldPath);
+
+        // return new file
+        return {
+            filename: file.originalFilename,
+            filepath: newPath,
+            size: file.size,
+            type: path.extname(file.originalFilename),
+        };
+    } catch (error) {
         return null;
     }
-
-    // get old path
-    const oldPath = file.filepath;
-
-    // define new path
-    const newPath = path.join(__dirname, '../public/uploads/logos/', file.originalFilename);
-
-    // check if file exists
-    const exists = await fs
-        .access(newPath)
-        .then(() => true)
-        .catch(() => false);
-
-    // remove old file
-    if (exists) {
-        await fs.unlink(newPath);
-    }
-
-    // upload file
-    await fs.rename(oldPath, newPath);
-
-    // return new file
-    return {
-        filename: file.originalFilename,
-        filepath: newPath,
-        size: file.size,
-        type: path.extname(file.originalFilename),
-    };
 };
