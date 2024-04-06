e36142645e6819c78bab62dca42e212a003c99b1
Amin Babu <amin.babu.bd@gmail.com>
2024-04-06 17:28:38 +0600

    Add _media-files.js for handling media file types

---
+++ constants/_media-files.js
@@ -0,0 +1,18 @@
+/**
+ * @file constants/_media-files.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 06 April, 2024
+ * @update_date 06 April, 2024
+ */
+
+// media file types
+const BRAND_LOGO_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/svg+xml'];
+const FAVICON_TYPES = ['image/png', 'image/x-icon'];
+
+// export constants
+module.exports = {
+    BRAND_LOGO_TYPES,
+    FAVICON_TYPES,
+};
