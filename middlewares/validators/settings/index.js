b7c2d6b2a9986ad85419e9ccbe6881aed5da2f0b
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 20:14:23 +0600

    Update date and update_date in settings validators

--- middlewares/validators/settings/index.js
+++ middlewares/validators/settings/index.js
@@ -3,8 +3,8 @@
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 04 April, 2024
- * @update_date 04 April, 2024
+ * @date 07 April, 2024
+ * @update_date 07 April, 2024
  */
 
 // export all settings validators
