79fe0ccf33d890f89012fe6532351037aab8f57d
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 14:37:41 +0600

    Add HALF_MEGA_BYTE constant

--- constants/_globals.js
+++ constants/_globals.js
@@ -12,6 +12,7 @@ const DEFAULT_PAGE_LIMIT = 10;
 const DEFAULT_PAGE_NUMBER = 1;
 
 // file size constants
+const HALF_MEGA_BYTE = 512 * 1024;
 const ONE_MEGA_BYTE = 1024 * 1024;
 const MIN_FILE_SIZE = ONE_MEGA_BYTE;
 const MAX_FILE_SIZE = 10 * ONE_MEGA_BYTE;
@@ -19,6 +20,7 @@ const DEFAULT_FILE_SIZE = 5 * ONE_MEGA_BYTE;
 
 // export constants
 module.exports = {
+    HALF_MEGA_BYTE,
     ONE_MEGA_BYTE,
     DEFAULT_PAGE_LIMIT,
     DEFAULT_PAGE_NUMBER,
