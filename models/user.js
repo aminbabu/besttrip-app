573c0127dc4020ee57cb14850ed40c5b0fd3f849
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 22:30:44 +0600

    Add address fields to user model

--- models/user.js
+++ models/user.js
@@ -58,6 +58,21 @@ const userSchema = new mongoose.Schema(
             enum: USER_STATUS,
             default: 'active',
         },
+        address: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        state: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        postalCode: {
+            type: String,
+        },
     },
     {
         timestamps: true,
