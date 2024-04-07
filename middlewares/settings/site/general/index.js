81a8f293797b2afb0e0a57b28c6c3504a4183809
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 19:10:32 +0600

    Add general settings middleware

---
+++ middlewares/settings/site/general/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /middlewares/settings/site/general/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 07 April, 2024
+ * @update_date 07 April, 2024
+ */
+
+// export all settings middlewares
+module.exports = {
+    generalSettingsUploader: require('./general-settings-uploader'),
+};
