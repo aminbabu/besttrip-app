bf2af2b0cf243e7dbdbf36cb7af35455d877152e
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 00:28:02 +0600

    Remove console.log statement in register.js

--- controllers/auth/users/register.js
+++ controllers/auth/users/register.js
@@ -14,7 +14,6 @@ const { generateToken, sendEmail } = require('../../../utils');
 
 // export register a new user controller
 module.exports = async (req, res, next) => {
-    console.log(req.body);
     try {
         // get user input
         const { name, email, phone, password } = req.body;
