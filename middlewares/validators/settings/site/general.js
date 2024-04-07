3b944bc98a045ef992c35ba340afd8b757f356be
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 11:42:34 +0600

    Update site general settings validator

--- middlewares/validators/settings/site/general.js
+++ middlewares/validators/settings/site/general.js
@@ -10,20 +10,15 @@
 // dependencies
 const { body } = require('express-validator');
 const { expressValidator } = require('../../../../handlers/errors');
-const validateImage = require('../../../../utils/validate-image');
-const formDataParser = require('../../../../utils/form-data-parser');
-const { BRAND_LOGO_TYPES, FAVICON_TYPES } = require('../../../../constants/_media-files');
 
 // export general site settings validator
 module.exports = [
-    formDataParser,
-    body('logo').optional().custom(validateImage(BRAND_LOGO_TYPES)),
-    body('favicon').optional().custom(validateImage(FAVICON_TYPES)),
-    body('title').isArray({ min: 1, max: 1 }).withMessage('Title is required'),
-    body('domain').optional().isArray({ min: 1 }).withMessage('Domain is required'),
+    body('title')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('Title must be between 3 and 255 characters'),
+    body('domain').isArray({ min: 1 }).withMessage('Domain is required'),
     body('description')
-        .optional()
-        .isArray({ min: 1, max: 1 })
-        .withMessage('Description is required'),
+        .isLength({ min: 3, max: 500 })
+        .withMessage('Description must be between 3 and 255 characters'),
     expressValidator,
 ];
