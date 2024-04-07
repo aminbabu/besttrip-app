df9a53a02c47143f76b8dd5550c09716ca65db9c
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 14:31:17 +0600

    Refactor upload-media.js to use default file size and image types

--- utils/global/upload-media.js
+++ utils/global/upload-media.js
@@ -12,30 +12,44 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const { DEFAULT_FILE_SIZE } = require('../../constants/_globals');
+const { DEFAULT_IMAGE_TYPES } = require('../../constants/_media-files');
+
+// create default storage
+const createStorage = (distFolder) =>
+    multer.diskStorage({
+        destination: (req, file, cb) => {
+            const uploadPath = path.join(__dirname, '../../public/uploads', distFolder);
+            if (!fs.existsSync(uploadPath)) {
+                fs.mkdirSync(uploadPath, { recursive: true });
+            }
+            cb(null, uploadPath);
+        },
+        filename: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            const filename = uuidv4() + ext;
+            cb(null, filename);
+        },
+    });
 
 // export upload media files
-module.exports = (fileTypes, folder, storage) =>
+module.exports = (
+    fileSize = DEFAULT_FILE_SIZE,
+    mimeTypes = DEFAULT_IMAGE_TYPES,
+    distFolder = 'media',
+    storage = null
+) =>
     multer({
-        storage: storage({
-            destination: (req, file, cb) => {
-                const uploadPath = path.join(__dirname, `../../public/uploads/${folder}`);
-                if (!fs.existsSync(uploadPath)) {
-                    fs.mkdirSync(uploadPath, { recursive: true });
-                }
-                cb(null, uploadPath);
-            },
-            filename: (req, file, cb) => {
-                const ext = path.extname(file.originalname);
-                const filename = uuidv4() + ext;
-                cb(null, filename);
-            },
-        }),
+        storage: storage || createStorage(distFolder),
+        limits: {
+            fileSize,
+        },
         fileFilter: (req, file, cb) => {
             const ext = path.extname(file.originalname).toLowerCase();
-            if (fileTypes.includes(ext)) {
+            if (mimeTypes.includes(ext)) {
                 cb(null, true);
             } else {
-                cb(new Error(`Invalid file type. Only ${fileTypes.join(', ')} files are allowed.`));
+                cb(new Error(`Invalid file type. Only ${mimeTypes.join(', ')} files are allowed.`));
             }
         },
     });
