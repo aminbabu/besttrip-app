5baece9cf2a0b5e12c5c99dee04fa4d223d482d4
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 22:57:01 +0600

    Add update-all-customers-wallet controller

---
+++ controllers/customers/update-all-customers-wallet.js
@@ -0,0 +1,73 @@
+/**
+ * @file /controllers/customers/update-all-customers-wallet.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 03 April, 2024
+ * @update_date 04 April, 2024
+ */
+
+// dependencies
+const { Customer } = require('../../models');
+const { generateToken } = require('../../utils');
+
+// export update all customers wallet controller
+module.exports = async (req, res, next) => {
+    try {
+        // get the wallet object from the request body
+        const { wallet } = req.body;
+
+        // get all customers
+        const customers = await Customer.find();
+
+        // check if customers exist
+        if (!customers.length) {
+            return res.status(404).json({
+                message: 'No customers found',
+            });
+        }
+
+        // update all customers
+        customers.forEach(async (customerItem) => {
+            // get customer
+            const customer = customerItem;
+
+            // calculate wallet balance based on transaction type
+            if (wallet) {
+                switch (wallet.type) {
+                    case 'top-up':
+                        customer.wallet.balance += wallet.balance;
+                        break;
+                    case 'deduct':
+                        customer.wallet.balance -= wallet.balance;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // update customer
+            customer.set({
+                ...customer.toObject(),
+                wallet: {
+                    ...wallet,
+                    balance: customer.wallet.balance,
+                },
+            });
+
+            // save customer
+            await customer.save();
+        });
+
+        // generate token
+        const token = generateToken(req.user);
+
+        return res.status(200).json({
+            message: 'All customers wallet updated successfully',
+            customers,
+            token,
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
