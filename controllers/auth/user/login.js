c4fec1d2c580ba42625e0a92b808148784ebeb17
Amin Babu <amin.babu.bd@gmail.com>
2024-04-07 20:17:49 +0600

    Update login.js file

--- controllers/auth/user/login.js
+++ controllers/auth/user/login.js
@@ -4,11 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 22 March, 2024
+ * @update_date 0
  */
 
 // dependencies
-const { User } = require('../../../models/index');
+const { User } = require('../../../models');
 const { comparePassword, generateToken } = require('../../../utils');
 
 // export login user controller
