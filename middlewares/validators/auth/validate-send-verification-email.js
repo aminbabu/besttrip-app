a2076e8d23903eeb9df1a7cb1b0f1811d17484db
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 13:44:37 +0600

    Refactor validation in validate-send-verification-email.js

--- middlewares/validators/auth/validate-send-verification-email.js
+++ middlewares/validators/auth/validate-send-verification-email.js
@@ -8,17 +8,11 @@
  */
 
 // dependencies
-const { body, header } = require('express-validator');
+const { body } = require('express-validator');
 const { expressValidator } = require('../../../handlers/errors');
 
 // validate resend verification email
 module.exports = [
     body('email').normalizeEmail().isEmail().withMessage('Email is not valid'),
-    header('Authorization')
-        .exists()
-        .withMessage('Authorization header is missing')
-        .bail()
-        .isString()
-        .withMessage('Authorization header is not a string'),
     expressValidator,
 ];
