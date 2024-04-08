0f3aa65df3bf7349c2a944304cc7c3d8dc1e5282
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 12:06:46 +0600

    Update email and phone validation in register middleware

--- middlewares/validators/auth/validate-register.js
+++ middlewares/validators/auth/validate-register.js
@@ -14,8 +14,8 @@ const { expressValidator } = require('../../../handlers/errors');
 // validate register
 module.exports = [
     body('name').trim().isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
-    body('email').normalizeEmail().isEmail().withMessage('Email is not valid'),
-    body('phone').isMobilePhone().withMessage('Phone number is not valid'),
+    body('email').isEmail().normalizeEmail().withMessage('Email is not valid'),
+    body('phone').isMobilePhone('any').withMessage('Phone number is not valid'),
     body('password')
         .isLength({ min: 8 })
         .withMessage('Password must be at least 6 characters long'),
