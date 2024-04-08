4d0cc93ab72127a86392e8a18a1bda8f1a79a66e
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 13:35:58 +0600

    Normalize email address in login validation

--- middlewares/validators/auth/validate-login.js
+++ middlewares/validators/auth/validate-login.js
@@ -13,7 +13,7 @@ const { expressValidator } = require('../../../handlers/errors');
 
 // validate login
 module.exports = [
-    body('email').isEmail().withMessage('Email is not valid'),
+    body('email').normalizeEmail().isEmail().withMessage('Email is not valid'),
     body('password')
         .isLength({ min: 8 })
         .withMessage('Password must be at least 6 characters long'),
