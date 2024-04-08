559387e8096c8e88f1c66828585fb5f62aabbb5c
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 14:47:54 +0600

    Update verify-email controller to handle token validation and expiration

--- controllers/auth/customers/verify-email.js
+++ controllers/auth/customers/verify-email.js
@@ -4,29 +4,31 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 22 March, 2024
+ * @update_date 08 April, 2024
  */
 
 // dependencies
+const moment = require('moment');
+const { matchedData } = require('express-validator');
 const { confirmEmailVerification } = require('../../../mails');
-const { Customer } = require('../../../models');
+const { Customer, Token } = require('../../../models');
 const { verifyToken, sendEmail, generateToken } = require('../../../utils');
 
 // export verify email controller
 module.exports = async (req, res, next) => {
     try {
-        // get token from query
-        const { token } = req.query;
+        // get validated data
+        const { token } = matchedData(req);
 
         // verify token
-        const payload = verifyToken(token);
+        const verifiedToken = verifyToken(token);
 
         // check if token is valid
-        const customer = await Customer.findById(payload.user._id);
+        const customer = await Customer.findById(verifiedToken.user._id);
 
         // check if customer exists
         if (!customer) {
-            return res.status(400).json({ message: 'Invalid token' });
+            return res.status(404).json({ message: 'Customer not found' });
         }
 
         // check if customer is already verified
@@ -38,11 +40,27 @@ module.exports = async (req, res, next) => {
         customer.isVerified = true;
         await customer.save();
 
+        // get existing tokens
+        const tokens = await Token.find({
+            customer: customer._id,
+            type: 'verify-email',
+        });
+
+        // delete existing tokens
+        await Promise.all(
+            tokens.map(
+                (tokenItem) =>
+                    tokenItem.type === 'verify-email' &&
+                    moment(tokenItem.expires).isBefore(moment()) && // check if token is expired
+                    tokenItem.deleteOne()
+            )
+        );
+
         // generate token
-        const newToken = generateToken(customer);
+        const newToken = generateToken(customer.toObject());
 
         // send email
-        const info = await confirmEmailVerification(customer);
+        const info = await confirmEmailVerification(customer.toObject());
         await sendEmail(info.to, info.subject, info.text, info.html, info.attachments);
 
         // set token in response
@@ -50,7 +68,7 @@ module.exports = async (req, res, next) => {
 
         // send response
         return res.status(200).json({
-            message: 'Email verified',
+            message: 'Email verified successfully',
         });
     } catch (err) {
         return next(err);
