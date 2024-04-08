d72697aa54b5025a8773ec5a801c2d21fd41399e
Amin Babu <amin.babu.bd@gmail.com>
2024-04-08 13:32:27 +0600

    Fix phone number validation in register form

--- middlewares/validators/auth/validate-register.js
+++ middlewares/validators/auth/validate-register.js
@@ -15,7 +15,7 @@ const { expressValidator } = require('../../../handlers/errors');
 module.exports = [
     body('name').trim().isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
     body('email').isEmail().normalizeEmail().withMessage('Email is not valid'),
-    body('phone').isMobilePhone('any').withMessage('Phone number is not valid'),
+    body('phone').isMobilePhone().withMessage('Phone number is not valid'),
     body('password')
         .isLength({ min: 8 })
         .withMessage('Password must be at least 6 characters long'),
