94a6118b4f95680984cac267a35c409bc0874836
Amin Babu <amin.babu.bd@gmail.com>
2024-04-09 01:06:55 +0600

    Refactor get all customers controller to use aggregation

--- controllers/customers/get-all-customers.js
+++ controllers/customers/get-all-customers.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 29 March, 2024
- * @update_date 02 April, 2024
+ * @update_date 08 April, 2024
  */
 
 // dependencies
@@ -13,13 +13,31 @@ const { Customer } = require('../../models');
 // export get all customers controller
 module.exports = async (req, res, next) => {
     try {
-        // get all customers
-        const customers = await Customer.find();
+        // get all customers and total count using aggregation
+        const [{ customers, totalCount }] = await Customer.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    customers: { $push: '$$ROOT' },
+                    totalCount: { $sum: 1 },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    customers: 1,
+                    totalCount: 1,
+                },
+            },
+        ]);
+
+        console.log(JSON.stringify(customers, null, 2));
 
         // return response
         return res.status(200).json({
             message: 'Fetched customers successfully',
             customers,
+            totalCount,
         });
     } catch (error) {
         return next(error);
