143f5507a83b52f48b77c9d82698963c3a1f7b09
Amin Babu <amin.babu.bd@gmail.com>
2024-04-12 14:07:02 +0600

    Add avatar upload functionality and handle existing avatars

--- middlewares/files/upload-avatar.js
+++ middlewares/files/upload-avatar.js
@@ -8,18 +8,23 @@
  */
 
 // dependencies
+const fs = require('fs');
 const path = require('path');
+const { Customer } = require('../../models');
 
 // export upload avatar middleware
 module.exports =
     (dir = '/uploads') =>
     async (req, res, next) => {
         try {
+            // get id
+            const { id } = req.params;
+
             // get avatar
             const { avatar } = req.files;
 
             // prepare file path
-            const filePath = path.join('uploads/avatars/', `${dir}/${avatar.name}`);
+            const filePath = path.join('uploads/avatars/', `${dir}/${Date.now()}_${avatar.name}`);
             const uploadPath = path.join(__dirname, '../../public/', filePath);
 
             // move file to upload path
@@ -28,7 +33,21 @@ module.exports =
             // set file path to request body
             req.body.avatar = filePath;
 
-            // call next middleware
+            // get customer
+            const customer = await Customer.findById(id);
+
+            // check if customer exists
+            if (!customer) {
+                return res.status(404).json({ message: 'Customer not found' });
+            }
+
+            // check if customer has avatar
+            if (customer.avatar) {
+                // delete previous avatar
+                fs.unlinkSync(path.join(__dirname, '../../public/', customer.avatar));
+            }
+
+            // continue to next middleware
             return next();
         } catch (error) {
             return res.status(500).json({ message: error.message });
