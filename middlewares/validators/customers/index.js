5adc7f9fce839a8bafbadd74d7cb0f62e309273f
Amin Babu <amin.babu.bd@gmail.com>
2024-04-13 00:30:21 +0600

    Update customer validators and remove validateExistedEmail

--- middlewares/validators/customers/index.js
+++ middlewares/validators/customers/index.js
@@ -4,13 +4,12 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 April, 2024
- * @update_date 13 April, 2024
+ * @update_date 08 April, 2024
  */
 
 // export all validators
 module.exports = {
     validateCustomerId: require('./validate-customer-id'),
-    validateExistedEmail: require('./validate-existed-email'),
     validateCustomerWallet: require('./validate-customer-wallet'),
     validateCustomer: require('./validate-customer'),
     validateCustomerSelf: require('./validate-customer-self'),
