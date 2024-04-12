65f2b72c2f8fb50cb2a63ac724986eceee3ea7ab
Amin Babu <amin.babu.bd@gmail.com>
2024-04-12 10:45:00 +0600

    Add global avatar validation middleware

---
+++ middlewares/validators/global/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file middlewares/validators/global/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 12 April, 2024
+ * @update_date 12 April, 2024
+ */
+
+// export all global validators
+module.exports = {
+    validateAvatar: require('./validate-avatar'),
+};
---
+++ middlewares/validators/global/validate-avatar.js
@@ -0,0 +1,41 @@
+/**
+ * @file middlewares/validators/global/validate-avatar.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 12 April, 2024
+ * @update_date 12 April, 2024
+ */
+
+// dependencies
+const { DEFAULT_IMAGE_TYPES, HALF_MEGA_BYTE } = require('../../../constants');
+
+// export validate avatar middleware
+module.exports = (req, res, next) => {
+    // check if file is not uploaded
+    if (!req.files || !req.files.avatar) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please upload an avatar',
+        });
+    }
+
+    // check if file is not an image of type jpeg, jpg, png
+    if (!DEFAULT_IMAGE_TYPES.includes(req.files.avatar.mimetype)) {
+        return res.status(400).json({
+            success: false,
+            message: `Please upload an image of type ${DEFAULT_IMAGE_TYPES.join(', ')}`,
+        });
+    }
+
+    // check if file size is greater than 5MB
+    if (req.files.avatar.size > HALF_MEGA_BYTE) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please upload an image less than 5MB',
+        });
+    }
+
+    // continue to next middleware
+    return next();
+};
