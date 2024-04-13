46f6209b7d7527691687c49df9f130d4cd571a5d
Amin Babu <amin.babu.bd@gmail.com>
2024-04-13 23:00:45 +0600

    Update site general settings validator

--- middlewares/validators/settings/site/general/index.js
+++ middlewares/validators/settings/site/general/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /middlewares/validators/settings/site/index.js
+ * @file /middlewares/validators/settings/site/general/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 04 April, 2024
- * @update_date 04 April, 2024
+ * @update_date 13 April, 2024
  */
 
 // export all settings validators
