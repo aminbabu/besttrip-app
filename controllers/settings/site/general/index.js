3fde3dee07a17974f914a02ce0d099ba8fd9f42b
Amin Babu <amin.babu.bd@gmail.com>
2024-04-13 22:52:24 +0600

    Update general settings controller

--- controllers/settings/site/general/index.js
+++ controllers/settings/site/general/index.js
@@ -4,10 +4,10 @@
  * @version 0.0.0
  * @author best-trip
  * @date 04 April, 2024
- * @update_date 07 April, 2024
+ * @update_date 13 April, 2024
  */
 
-// export all settings controllers
+// export general settings controllers
 module.exports = {
     getGeneralSettings: require('./getGeneralSettings'),
     updateGeneralSettings: require('./updateGeneralSettings'),
