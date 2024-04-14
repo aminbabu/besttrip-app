9b921be8b9161b15cdaafd593d31ae755c14dde8
Amin Babu <amin.babu.bd@gmail.com>
2024-04-14 11:14:57 +0600

    Update user validators export date

--- middlewares/validators/users/index.js
+++ middlewares/validators/users/index.js
@@ -4,10 +4,10 @@
  * @version 0.0.0
  * @author best-trip
  * @date 08 April, 2024
- * @update_date 10 April, 2024
+ * @update_date 14 April, 2024
  */
 
-// export all validators
+// export user validators
 module.exports = {
     validateUserId: require('./validate-user-id'),
     validateUser: require('./validate-user'),
