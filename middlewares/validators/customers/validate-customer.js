ed35c7cc6e85f961824c7c45a9b7288583381b15
Amin Babu <amin.babu.bd@gmail.com>
2024-04-14 11:12:32 +0600

    Refactor customer validation middleware

--- middlewares/validators/customers/validate-customer.js
+++ middlewares/validators/customers/validate-customer.js
@@ -11,7 +11,7 @@
 const { customerSchema } = require('../../../schemas/zod/customers');
 const { zodErrorHandler } = require('../../../handlers/errors');
 
-// update customer validator
+// export validate customer middleware
 module.exports = (req, res, next) => {
     // validate request body
     const { data, error, success } = customerSchema.safeParse({ ...req.params, ...req.body });
