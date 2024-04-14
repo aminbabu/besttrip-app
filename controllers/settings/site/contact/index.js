336b4192ca6467126f888bb6c3bb68fa39786b57
Amin Babu <amin.babu.bd@gmail.com>
2024-04-14 23:19:04 +0600

    Refactor contact settings controllers

--- controllers/settings/site/contact/index.js
+++ controllers/settings/site/contact/index.js
@@ -4,11 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 13 April, 2024
- * @update_date 13 April, 2024
+ * @update_date 14 April, 2024
  */
 
 // export contact settings controllers
 module.exports = {
-    getContactSettings: require('./getContactSettings'),
-    updateContactSettings: require('./updateContactSettings'),
+    getContactSettings: require('./get-contact-settings'),
+    updateContactSettings: require('./update-contact-settings'),
 };
