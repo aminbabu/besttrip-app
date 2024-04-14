2b1789fe5ca677ca2d3126fd201b1f02c41daa02
Amin Babu <amin.babu.bd@gmail.com>
2024-04-14 10:41:04 +0600

    Update customer validation middleware

--- middlewares/validators/customers/validate-customer-id.js
+++ middlewares/validators/customers/validate-customer-id.js
@@ -4,11 +4,29 @@
  * @version 0.0.0
  * @author best-trip
  * @date 08 April, 2024
- * @update_date 08 April, 2024
+ * @update_date 14 April, 2024
  */
 
 // dependencies
-const { param } = require('express-validator');
+const { customerSchema } = require('../../../schemas/zod/customers');
+const { zodErrorHandler } = require('../../../handlers/errors');
 
-// export validate customer by id middleware
-module.exports = [param('id').exists().isMongoId().withMessage('Invalid customer id')];
+// update customer validator
+module.exports = (req, res, next) => {
+    // validate request body
+    const { data, error, success } = customerSchema
+        .pick({ id: true })
+        .safeParse({ ...req.params, ...req.body });
+
+    // check for errors
+    if (!success) {
+        // return error response
+        return zodErrorHandler(res, error);
+    }
+
+    // set validated data
+    req.body = data;
+
+    // proceed to next middleware
+    return next();
+};
