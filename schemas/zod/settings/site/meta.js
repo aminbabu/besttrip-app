b4ce161ab80223aff8c06ed4606b37c3b9c9fae3
Amin Babu <amin.babu.bd@gmail.com>
2024-04-17 23:07:08 +0600

    Refactor meta data schema in site settings

--- schemas/zod/settings/site/meta.js
+++ schemas/zod/settings/site/meta.js
@@ -12,31 +12,33 @@ const { isMongoId } = require('validator');
 const { z } = require('zod');
 
 // export meta data schema
-(module.exports = z.object({
-    id: z
-        .string({
-            required_error: 'Id is required.',
-            invalid_type_error: 'Please provide a valid id.',
-        })
-        .refine((id) => isMongoId(id), {
-            message: 'Please provide a valid id.',
-        }),
-    name: z
-        .string({
-            required_error: 'Meta name is required',
-            invalid_type_error: 'Meta name must be a string',
-        })
-        .trim()
-        .min(3, {
-            message: 'Meta name must be at least 3 characters',
-        }),
-    content: z
-        .string({
-            required_error: 'Meta content is required',
-            invalid_type_error: 'Meta content must be a string',
-        })
-        .trim()
-        .min(3, {
-            message: 'Meta content must be at least 3 characters',
-        }),
-})).strict();
+module.exports = z
+    .object({
+        id: z
+            .string({
+                required_error: 'Id is required.',
+                invalid_type_error: 'Please provide a valid id.',
+            })
+            .refine((id) => isMongoId(id), {
+                message: 'Please provide a valid id.',
+            }),
+        name: z
+            .string({
+                required_error: 'Meta name is required',
+                invalid_type_error: 'Meta name must be a string',
+            })
+            .trim()
+            .min(3, {
+                message: 'Meta name must be at least 3 characters',
+            }),
+        content: z
+            .string({
+                required_error: 'Meta content is required',
+                invalid_type_error: 'Meta content must be a string',
+            })
+            .trim()
+            .min(3, {
+                message: 'Meta content must be at least 3 characters',
+            }),
+    })
+    .strict();
