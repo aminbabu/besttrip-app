8bbf536e24a59658912fca09e7de1ada2e387900
Amin Babu <amin.babu.bd@gmail.com>
2024-04-17 12:31:47 +0600

    Update contact settings file

--- models/settings/site/contact-settings.js
+++ models/settings/site/contact-settings.js
@@ -1,10 +1,10 @@
 /**
- * @file /models/settings/site/ContactSettings.js
+ * @file /models/settings/site/contact-settings.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 13 April, 2024
- * @update_date 14 April, 2024
+ * @update_date 17 April, 2024
  */
 
 // dependencies
