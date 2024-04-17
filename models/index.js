76a71f6bbc7f46b26b8078b38bd98a663caf218a
Amin Babu <amin.babu.bd@gmail.com>
2024-04-17 12:45:12 +0600

    Update models and add MetaSettings model

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 16 April, 2024
+ * @update_date 17 April, 2024
  */
 
 // export all models
@@ -15,4 +15,5 @@ module.exports = {
     GeneralSettings: require('./settings/site/general-settings'),
     ContactSettings: require('./settings/site/contact-settings'),
     PolicySettings: require('./settings/site/policy-settings'),
+    MetaSettings: require('./settings/site/meta-settings'),
 };
---
+++ models/settings/site/meta-settings.js
@@ -0,0 +1,15 @@
+/**
+ * @file /models/settings/site/meta-data.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 17 April, 2024
+ * @update_date 17 April, 2024
+ */
+
+// dependencies
+const { model } = require('mongoose');
+const { metaSettingsSchema } = require('../../../schemas/mongoose');
+
+// export model
+module.exports = model('MetaSettings', metaSettingsSchema);
