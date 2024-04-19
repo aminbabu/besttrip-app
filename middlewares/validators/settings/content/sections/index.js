d0ba89f89054762a95fb11b4d4dcca189e3b8130
Amin Babu <amin.babu.bd@gmail.com>
2024-04-19 11:05:05 +0600

    Update content sections settings validator middlewares

--- middlewares/validators/settings/content/sections/index.js
+++ middlewares/validators/settings/content/sections/index.js
@@ -3,12 +3,11 @@
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 17 April, 2024
- * @update_date 17 April, 2024
+ * @date 19 April, 2024
+ * @update_date 19 April, 2024
  */
 
 // export content sections settings validator middlewares
 module.exports = {
-    validateContentSectionId: require('./validate-content-section-id'),
     validateContentSection: require('./validate-content-section'),
 };
