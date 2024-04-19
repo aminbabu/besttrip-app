d53360fe0904445f601f0ee9e14a8de6778a42c2
Amin Babu <amin.babu.bd@gmail.com>
2024-04-20 05:51:23 +0600

    Update exclusive-offers and hotel-offers controller files

--- controllers/settings/content/exclusive-offers/index.js
+++ controllers/settings/content/exclusive-offers/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /controllers/settings/content/exclusive-offers/get-exclusive-offers.js
+ * @file /controllers/settings/content/exclusive-offers/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 19 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 20 April, 2024
  */
 
 // export exclusive offers controllers
--- controllers/settings/content/hotel-offers/index.js
+++ controllers/settings/content/hotel-offers/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /controllers/settings/content/hotel-offers/get-hotel-offers.js
+ * @file /controllers/settings/content/hotel-offers/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 19 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 20 April, 2024
  */
 
 // export hotel offers controllers
