3b2a4f890cf0dfecb0bd1e0993c8f6eb4ede946e
Amin Babu <amin.babu.bd@gmail.com>
2024-04-20 14:40:36 +0600

    Update payment controller file

--- controllers/settings/payments/index.js
+++ controllers/settings/payments/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /controllers/payments/index.js
+ * @file /controllers/settings/payments/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 17 April, 2024
- * @update_date 17 April 2024
+ * @update_date 20 April 2024
  */
 
 // export payments controllers
