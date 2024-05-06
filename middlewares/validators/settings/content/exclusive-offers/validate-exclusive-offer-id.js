b4a15ca7534520820f2de01106275f486ad2bea5
Amin Babu <amin.babu.bd@gmail.com>
2024-05-06 18:00:48 +0600

    Update exclusive offer id validation to use req.params

--- middlewares/validators/settings/content/exclusive-offers/validate-exclusive-offer-id.js
+++ middlewares/validators/settings/content/exclusive-offers/validate-exclusive-offer-id.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 19 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 06 May, 2024
  */
 
 // dependencies
@@ -14,7 +14,7 @@ const { zodErrorHandler } = require('../../../../../handlers/errors');
 // export exclusive offer id validator middleware
 module.exports = (req, res, next) => {
     // validate request body
-    const { error, success } = exclusiveOfferSchema.pick({ id: true }).safeParse(req.body);
+    const { error, success } = exclusiveOfferSchema.pick({ id: true }).safeParse(req.params);
 
     // check for errors
     if (!success) {
