b965299f18e9f4bd301e143b70fc31323dcdc76a
Amin Babu <amin.babu.bd@gmail.com>
2024-05-08 14:25:08 +0600

    Update umrah offer id validation to use req.params

--- middlewares/validators/settings/content/umrah-offers/validate-umrah-offer-id.js
+++ middlewares/validators/settings/content/umrah-offers/validate-umrah-offer-id.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 20 April, 2024
- * @update_date 20 April, 2024
+ * @update_date 08 May, 2024
  */
 
 // dependencies
@@ -14,7 +14,7 @@ const { zodErrorHandler } = require('../../../../../handlers/errors');
 // export umrah offer id validator middleware
 module.exports = (req, res, next) => {
     // validate request body
-    const { error, success } = umrahOfferSchema.pick({ id: true }).safeParse(req.body);
+    const { error, success } = umrahOfferSchema.pick({ id: true }).safeParse(req.params);
 
     // check for errors
     if (!success) {
