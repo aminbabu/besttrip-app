c60870733dca04c01eec7e2f46c682328a6df1d7
Amin Babu <amin.babu.bd@gmail.com>
2024-05-10 13:28:05 +0600

    Add umrah package schedules constant

--- constants/umrah-pacakges.js
+++ constants/umrah-pacakges.js
@@ -4,11 +4,27 @@
  * @version 0.0.0
  * @author best-trip
  * @date 25 April, 2024
- * @update_date 09 May, 2024
+ * @update_date 10 May, 2024
  */
 
 // umrah package package status
 const UMDAH_PACKAGE_STATUS = ['active', 'disabled'];
+
+// umrah package package schudle
+const UMDAH_PACKAGE_SCHEDULES = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',
+];
 
 // umrah package package inclusions
 const UMDAH_PACKAGE_INCLUSIONS = ['flight', 'hotel', 'visa', 'transport', 'food'];
@@ -22,6 +38,7 @@ const UMDAH_PACKAGE_OUTBOUND_FLIGHT_STOPS = [0, 1, 2];
 // export umdah packages constants
 module.exports = {
     UMDAH_PACKAGE_STATUS,
+    UMDAH_PACKAGE_SCHEDULES,
     UMDAH_PACKAGE_INCLUSIONS,
     UMDAH_PACKAGE_TYPES,
     UMDAH_PACKAGE_OUTBOUND_FLIGHT_STOPS,
