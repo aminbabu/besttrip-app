7d4f200006a427d1e2909ad905595d278eb40b14
Amin Babu <amin.babu.bd@gmail.com>
2024-05-10 13:10:46 +0600

    Refactor thumbnail upload logic in upload-makka-hotel-gallery middleware

--- middlewares/umrah/packages/upload-makka-hotel-gallery.js
+++ middlewares/umrah/packages/upload-makka-hotel-gallery.js
@@ -36,20 +36,20 @@ module.exports =
 
         // prepare file path
         const updateExtraThumbnails = makkahHotelExtraThumbnails.map((thumbnail) => {
-            const newThumbnail = { ...thumbnail };
+            const updatedThumbnail = { ...thumbnail };
             const thumbnailPath = path.join(
                 'uploads/',
-                `${dir}/${Date.now()}_${newThumbnail.name}`
+                `${dir}/${Date.now()}_${updatedThumbnail.name}`
             );
             const uploadLogoPath = path.join(__dirname, '../../../public/', thumbnailPath);
 
             // move file to upload path
-            newThumbnail.mv(uploadLogoPath);
+            updatedThumbnail.mv(uploadLogoPath);
 
             // set file path to thumbnail object
-            newThumbnail.path = thumbnailPath;
+            updatedThumbnail.path = thumbnailPath;
 
-            return newThumbnail;
+            return updatedThumbnail;
         });
 
         // set file path to request body
