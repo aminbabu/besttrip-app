a05e5256b6884a283d798c693442aa4c63b8785c
Amin Babu <amin.babu.bd@gmail.com>
2024-05-10 14:47:24 +0600

    Add visa constants

--- constants/index.js
+++ constants/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 07 April, 2024
- * @update_date 09 May, 2024
+ * @update_date 10 May, 2024
  */
 
 // export constants
@@ -27,4 +27,5 @@ module.exports = {
     ...require('./umrah-pacakges'),
     ...require('./umrah-package-durations'),
     ...require('./umrah-package-types'),
+    ...require('./visa'),
 };
---
+++ constants/visa.js
@@ -0,0 +1,20 @@
+/**
+ * @file /constants/visa.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 10 May, 2024
+ * @update_date 10 May, 2024
+ */
+
+// visa status
+const VISA_STATUS = ['active', 'disabled'];
+
+// visa types
+const VISA_TYPES = ['tourist', 'umrah', 'ziyarah', 'hajj'];
+
+// export visa constants
+module.exports = {
+    VISA_STATUS,
+    VISA_TYPES,
+};
