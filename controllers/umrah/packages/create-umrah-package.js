a2431c9189584e554378c592ac0c440602e6efe8
Amin Babu <amin.babu.bd@gmail.com>
2024-05-17 17:30:52 +0600

    Update umrah package creation logic

--- controllers/umrah/packages/create-umrah-package.js
+++ controllers/umrah/packages/create-umrah-package.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 25 April, 2024
- * @update_date 09 May, 2024
+ * @update_date 17 May, 2024
  */
 
 // dependencies
@@ -15,9 +15,39 @@ module.exports = async (req, res, next) => {
     try {
         // get validated data
         const validatedData = req.body;
+        const {
+            thumbnail,
+            extraThumbnails,
+            makkahHotelThumbnail,
+            makkahHotelExtraThumbnails,
+            madinahHotelThumbnail,
+            madinahhHotelExtraThumbnails,
+            itineraryDays,
+            umrahThumbnail,
+        } = req.files;
+
+        console.log(JSON.stringify(validatedData, null, 2));
 
         // create umrah package
-        const umrahPackage = new UmrahPackage(validatedData);
+        const umrahPackage = new UmrahPackage({
+            ...validatedData,
+            schedule: validatedData.schedule.toLowerCase(),
+            thumbnail: thumbnail.path,
+            extraThumbnails: extraThumbnails?.map((extraThumbnail) => extraThumbnail.path),
+            makkahHotelThumbnail: makkahHotelThumbnail.path,
+            makkahHotelExtraThumbnails: makkahHotelExtraThumbnails?.map(
+                (makkahHotelExtraThumbnail) => makkahHotelExtraThumbnail.path
+            ),
+            madinahHotelThumbnail: madinahHotelThumbnail.path,
+            madinahhHotelExtraThumbnails: madinahhHotelExtraThumbnails?.map(
+                (madinahhHotelExtraThumbnail) => madinahhHotelExtraThumbnail.path
+            ),
+            itineraryDays: itineraryDays?.map((itineraryDay) => ({
+                ...itineraryDay,
+                thumbnail: itineraryDay.thumbnail.path,
+            })),
+            umrahThumbnail: umrahThumbnail.path,
+        });
 
         // save umrah package
         await umrahPackage.save();
@@ -28,6 +58,7 @@ module.exports = async (req, res, next) => {
             umrahPackage,
         });
     } catch (error) {
+        console.log(error);
         return next(error);
     }
 };
