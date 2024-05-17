11b17df11f69b00f1d03bf128aa3f2cebff2be8c
Amin Babu <amin.babu.bd@gmail.com>
2024-05-17 08:36:51 +0600

    Refactor day wise itinerary thumbnails validation

--- middlewares/validators/umrah/packages/validate-day-wise-itinerary-thumbnails.js
+++ middlewares/validators/umrah/packages/validate-day-wise-itinerary-thumbnails.js
@@ -1,11 +1,10 @@
 /**
- * @file
- * /middlewares/validation/umrah/extanets/validate-day-wise-itinerary-thumbnails.js
+ * @file /middlewares/validation/umrah/extanets/validate-day-wise-itinerary-thumbnails.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 15 May, 2024
- * @update_date 15 May, 2024
+ * @update_date 17 May, 2024
  */
 
 // dependencies
@@ -13,6 +12,10 @@ const { DEFAULT_IMAGE_TYPES, ONE_MEGA_BYTE } = require('../../../../constants');
 
 // export umrah day wise itinerary thumbnails validator
 module.exports = async (req, res, next) => {
+    let isThumbnailArray = false;
+    let isInValidImageType = false;
+    let isInValidImageSize = false;
+
     // get day wise itinerary thumbnails from request
     const { itineraryDays } = req.files || {};
 
@@ -21,26 +24,48 @@ module.exports = async (req, res, next) => {
         return next();
     }
 
-    // check if day wise itenerary thumbnail is not an image of type jpg, jpeg, png
-    itineraryDays.forEach(
-        (image) =>
-            !DEFAULT_IMAGE_TYPES.includes(image.mimetype) &&
-            res.status(400).json({
-                message: `Please upload a valid day wise itinerary image of type ${DEFAULT_IMAGE_TYPES.join(', ')}`,
-            })
-    );
+    // Check each itinerary thumbnail
+    itineraryDays.forEach((itinerary) => {
+        // Check if thumbnail is not an array
+        if (Array.isArray(itinerary?.thumbnail)) {
+            isThumbnailArray = true;
+        }
 
-    // check if each image size is greater than 1 MB
-    itineraryDays.forEach(
-        (image) =>
-            image.size > ONE_MEGA_BYTE &&
-            res.status(400).json({
-                message: `Please upload day wise itinerary images of size less than ${(
-                    ONE_MEGA_BYTE / ONE_MEGA_BYTE
-                ).toFixed(2)} MB`,
-            })
-    );
+        // Check if thumbnail is not an image of type jpg, jpeg, png
+        if (!DEFAULT_IMAGE_TYPES.includes(itinerary?.thumbnail.mimetype)) {
+            isInValidImageType = true;
+        }
 
-    // proceed to next middleware
+        // Check if image size is greater than 1 MB
+        if (itinerary?.thumbnail.size > ONE_MEGA_BYTE) {
+            isInValidImageSize = true;
+        }
+
+        // Add return statement at the end of the arrow function
+        return null;
+    });
+
+    // Check if all checks pass
+    if (isThumbnailArray) {
+        return res.status(400).json({
+            message: 'Please upload valid day wise itinerary images instead of array of images',
+        });
+    }
+
+    if (isInValidImageType) {
+        return res.status(400).json({
+            message: `Please upload valid day wise itinerary images of type ${DEFAULT_IMAGE_TYPES.join(', ')}`,
+        });
+    }
+
+    if (isInValidImageSize) {
+        return res.status(400).json({
+            message: `Please upload day wise itinerary images of size less than ${(
+                ONE_MEGA_BYTE / ONE_MEGA_BYTE
+            ).toFixed(2)} MB`,
+        });
+    }
+
+    // Proceed to next middleware if all checks pass
     return next();
 };
