a141d94b8b37ee38eeb3140e3a890df594d95a0d
Amin Babu <amin.babu.bd@gmail.com>
2024-05-17 20:01:51 +0600

    Update update_date in config/index.js

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 07 April, 2024
- * @update_date 17 April, 2024
+ * @update_date 17 May, 2024
  */
 
 // export all configurations
@@ -12,8 +12,4 @@ module.exports = {
     createDBConnection: require('./database'),
     env: require('./env'),
     expressFileUploadConf: require('./express-fileupload'),
-    createParentPath: true,
-    safeFileNames: true,
-    preserveExtension: true,
-    preservePath: true,
 };
