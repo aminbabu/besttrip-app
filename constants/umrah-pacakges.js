299b5ed05d64a19e94bdfa7733370061541bcc8f
Amin Babu <amin.babu.bd@gmail.com>
2024-05-17 17:30:42 +0600

    Update umrah-pacakges.js with new boolean constant

--- constants/umrah-pacakges.js
+++ constants/umrah-pacakges.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 25 April, 2024
- * @update_date 14 May, 2024
+ * @update_date 17 May, 2024
  */
 
 // umrah package package status
@@ -35,6 +35,9 @@ const UMRAH_PACKAGE_OUTBOUND_FLIGHT_STOPS = [0, 1, 2];
 // umrah package distance units
 const UMRAH_PACKAGE_DISTANCE_UNITS = ['m', 'km', 'mile'];
 
+// umrah package includes
+const UMRAH_PACKAGE_BOOLEAN = ['yes', 'no'];
+
 // export umrah packages constants
 module.exports = {
     UMRAH_PACKAGE_STATUS,
@@ -42,4 +45,5 @@ module.exports = {
     UMRAH_PACKAGE_INCLUSIONS,
     UMRAH_PACKAGE_OUTBOUND_FLIGHT_STOPS,
     UMRAH_PACKAGE_DISTANCE_UNITS,
+    UMRAH_PACKAGE_BOOLEAN,
 };
