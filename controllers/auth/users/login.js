5aff7bd018b666c164dbf0ee939e0b66dca601bc
Amin Babu <amin.babu.bd@gmail.com>
2024-05-22 17:26:15 +0600

    Refactor login, forgot-password, and reset-password controllers

--- controllers/auth/users/login.js
+++ controllers/auth/users/login.js
@@ -1,63 +1,17 @@
 /**
- * @file /controllers/auth/users/index.js
+ * @file /controllers/auth/users/login.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 18 March, 2024
- * @update_date 10 April, 2024
+ * @date 22 May, 2024
+ * @update_date 22 May, 2024
  */
 
-// dependencies
-const { User } = require('../../../models');
-const { comparePassword, generateToken } = require('../../../utils');
-
-// export login user controller
+// export login view controller
 module.exports = async (req, res, next) => {
     try {
-        // get validated data
-        const { email, password } = req.body;
-
-        // check if user exists
-        const user = await User.findOne({ email }).select('+password');
-
-        if (!user) {
-            return res.status(400).json({
-                message: 'Invalid email or password',
-            });
-        }
-
-        // check if user status is active
-        if (user.status !== 'active') {
-            return res.status(400).json({
-                message: 'User is not active',
-            });
-        }
-
-        // convert user to object
-        const userObject = user.toObject();
-
-        // compare password
-        const match = await comparePassword(password, userObject.password);
-
-        if (!match) {
-            return res.status(400).json({
-                message: 'Invalid email or password',
-            });
-        }
-
-        // remove password from user object
-        delete userObject.password;
-
-        // generate token
-        const token = generateToken(userObject);
-
-        // set token in response
-        res.set('authorization', `Bearer ${token}`);
-
-        // return response
-        return res.status(200).json({
-            message: 'Login successful',
-        });
+        // render register view
+        return res.render('auth/users/login');
     } catch (error) {
         return next(error);
     }
