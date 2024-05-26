b849b0669a2a2298b3d227d21d1d11699a292a4b
Amin Babu <amin.babu.bd@gmail.com>
2024-05-26 23:06:10 +0600

    Add is-api-valid middleware

--- middlewares/auth/index.js
+++ middlewares/auth/index.js
@@ -4,11 +4,12 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 24 May, 2024
+ * @update_date 26 May, 2024
  */
 
 // export all middlewares
 module.exports = {
+    isApiValid: require('./is-api-valid'),
     isAuthorized: require('./is-authorized'),
     isNotAuthorized: require('./is-not-authorized'),
     isVerified: require('./is-verified'),
---
+++ middlewares/auth/is-api-valid.js
@@ -0,0 +1,24 @@
+/**
+ * @file /middlewares/auth/is-api-valid.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 26 May, 2024
+ * @update_date 26 May, 2024
+ */
+
+// dependencies
+const { FRONTEND_KEY, BACKEND_KEY } = require('../../config');
+
+// export api key validation middleware
+module.exports = (req, res, next) => {
+    // get api key
+    const apiKey = req.headers['x-api-key'];
+
+    // check if api key is valid
+    if (apiKey === FRONTEND_KEY || apiKey === BACKEND_KEY) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+};
