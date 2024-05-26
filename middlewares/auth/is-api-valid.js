1b7c867405d66867bb990dc7189d996168c5c433
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 05:38:48 +0600

    Update API key validation middleware

--- middlewares/auth/is-api-valid.js
+++ middlewares/auth/is-api-valid.js
@@ -4,28 +4,32 @@
  * @version 0.0.0
  * @author best-trip
  * @date 26 May, 2024
- * @update_date 26 May, 2024
+ * @update_date 28 May, 2024
  */
 
 // dependencies
-const { FRONTEND_KEY, BACKEND_KEY } = require('../../config');
+const { env } = require('../../config');
 
 // export api key validation middleware
 module.exports = (req, res, next) => {
     // get api key
     const apiKey = req.header('x-api-key');
+
+    // check if api key is exist
+    if (!apiKey) return res.status(401).json({ message: 'API key is required' });
+
     // check if api key is valid for frontend
-    if (apiKey === FRONTEND_KEY) {
+    if (apiKey === env.FRONTEND_KEY) {
         res.locals.api = 'frontend';
         return next();
     }
 
     // check if api key is valid for backend
-    if (apiKey === BACKEND_KEY) {
+    if (apiKey === env.BACKEND_KEY) {
         res.locals.api = 'backend';
         return next();
     }
 
-    // return unauthorized
-    return res.status(401).json({ message: 'Unauthorized' });
+    // return error if api key is invalid
+    return res.status(401).json({ message: 'Invalid API key' });
 };
