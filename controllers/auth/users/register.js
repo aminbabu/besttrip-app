70c0f718a09a3ec1d3a31722b9965e83c5aca8b8
Amin Babu <amin.babu.bd@gmail.com>
2024-05-26 22:32:45 +0600

    Update register view controller to redirect to home page

--- controllers/auth/users/register.js
+++ controllers/auth/users/register.js
@@ -4,14 +4,15 @@
  * @version 0.0.0
  * @author best-trip
  * @date 22 May, 2024
- * @update_date 22 May, 2024
+ * @update_date 26 May, 2024
  */
 
 // export register view controller
 module.exports = async (req, res, next) => {
     try {
         // render register view
-        return res.render('auth/users/register');
+        // return res.render('auth/users/register');
+        return res.redirect('/');
     } catch (error) {
         return next(error);
     }
