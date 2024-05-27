46dcb217ba29d979ceeab51d1a6204cf3a705932
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 06:24:42 +0600

    Add auth middlewares

---
+++ middlewares/api/auth/index.js
@@ -0,0 +1,15 @@
+/**
+ * @file /middlewares/auth/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 18 March, 2024
+ * @update_date 27 May, 2024
+ */
+
+// export all middlewares
+module.exports = {
+    isAuthorized: require('./is-authorized'),
+    isVerified: require('./is-verified'),
+    isAllowed: require('./is-allowed'),
+};
