efce6dcb7668bf84ffd89f5fe529d74173c04158
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 13:31:40 +0600

    Update admin routes to dashboard admin routes

--- routes/auth/users/index.js
+++ routes/auth/users/index.js
@@ -1,5 +1,5 @@
 /**
- * @file /routes/admin/index.js
+ * @file /routes/dashboard/admin/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
@@ -27,18 +27,18 @@ const { isAuthorized, isNotAuthorized } = require('../../../middlewares/auth');
 
 /**
  * @description redirect to login page
- * @param {string} path - /admin/
+ * @param {string} path - /dashboard/admin/
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - []
  * @returns {object} - router
  * @access public
  * @method GET
  */
-router.get('/', (req, res) => res.redirect('/admin/login'));
+router.get('/', (req, res) => res.redirect('/dashboard/admin/login'));
 
 /**
  * @description register a new user
- * @param {string} path - /admin/register
+ * @param {string} path - /dashboard/admin/register
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['register']
  * @returns {object} - router
@@ -49,7 +49,7 @@ router.get('/register', isNotAuthorized, register);
 
 /**
  * @description login a user
- * @param {string} path - /admin/login
+ * @param {string} path - /dashboard/admin/login
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['login']
  * @returns {object} - router
@@ -60,7 +60,7 @@ router.get('/login', isNotAuthorized, login);
 
 /**
  * @description forgot password
- * @param {string} path - /admin/forgot-password
+ * @param {string} path - /dashboard/admin/forgot-password
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['forgotPassword']
  * @returns {object} - router
@@ -71,7 +71,7 @@ router.get('/forgot-password', isNotAuthorized, forgotPassword);
 
 /**
  * @description reset password
- * @param {string} path - /admin/reset-password
+ * @param {string} path - /dashboard/admin/reset-password
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['resetPassword']
  * @returns {object} - router
@@ -82,7 +82,7 @@ router.get('/reset-password', isNotAuthorized, resetPassword);
 
 /**
  * @description send verification email
- * @param {string} path - /admin/send-verification-email
+ * @param {string} path - /dashboard/admin/send-verification-email
  * @param {function} controller - ['sendVerificationEmail']
  * @returns {object} - router
  * @access public
@@ -92,7 +92,7 @@ router.get('/send-verification-email', isAuthorized, sendVerificationEmail);
 
 /**
  * @description verify email
- * @param {string} path - /admin/verify-email
+ * @param {string} path - /dashboard/admin/verify-email
  * @param {function} controller - ['verifyEmail']
  * @returns {object} - router
  * @access public
@@ -102,7 +102,7 @@ router.get('/verify-email', verifyEmail);
 
 /**
  * @description logout user
- * @param {string} path - /admin/logout
+ * @param {string} path - /dashboard/admin/logout
  * @param {function} controller - ['logout']
  * @returns {object} - router
  * @access public
