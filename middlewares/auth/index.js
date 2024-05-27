dc0efff70ced873165f039d04d1d57f36254f931
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 06:11:34 +0600

    Update auth middleware with new isNotAllowed middleware

--- middlewares/auth/index.js
+++ middlewares/auth/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 26 May, 2024
+ * @update_date 27 May, 2024
  */
 
 // export all middlewares
@@ -14,4 +14,5 @@ module.exports = {
     isNotAuthorized: require('./is-not-authorized'),
     isVerified: require('./is-verified'),
     isAllowed: require('./is-allowed'),
+    isNotAllowed: require('./is-not-allowed'),
 };
