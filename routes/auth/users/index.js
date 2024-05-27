00261c73880b2c40b88fa66666e39422e15b728b
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 08:23:28 +0600

    Update admin routes and add logout functionality

--- routes/auth/users/index.js
+++ routes/auth/users/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /routes/auth/users/index.js
+ * @file /routes/admin/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
  * @date 22 May, 2024
- * @update_date 24 May, 2024
+ * @update_date 27 May, 2024
  */
 
 // dependencies
@@ -21,12 +21,24 @@ const {
     resetPassword,
     sendVerificationEmail,
     verifyEmail,
+    logout,
 } = require('../../../controllers/auth/users');
 const { isAuthorized, isNotAuthorized } = require('../../../middlewares/auth');
 
 /**
+ * @description redirect to login page
+ * @param {string} path - /admin/
+ * @param {function} middleware - ['isNotAuthorized']
+ * @param {function} controller - []
+ * @returns {object} - router
+ * @access public
+ * @method GET
+ */
+router.get('/', (req, res) => res.redirect('/admin/login'));
+
+/**
  * @description register a new user
- * @param {string} path - /auth/users/register
+ * @param {string} path - /admin/register
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['register']
  * @returns {object} - router
@@ -37,7 +49,7 @@ router.get('/register', isNotAuthorized, register);
 
 /**
  * @description login a user
- * @param {string} path - /auth/users/login
+ * @param {string} path - /admin/login
  * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['login']
  * @returns {object} - router
@@ -48,27 +60,29 @@ router.get('/login', isNotAuthorized, login);
 
 /**
  * @description forgot password
- * @param {string} path - /auth/users/forgot-password
+ * @param {string} path - /admin/forgot-password
+ * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['forgotPassword']
  * @returns {object} - router
  * @access public
  * @method GET
  */
-router.get('/forgot-password', forgotPassword);
+router.get('/forgot-password', isNotAuthorized, forgotPassword);
 
 /**
  * @description reset password
- * @param {string} path - /auth/users/reset-password
+ * @param {string} path - /admin/reset-password
+ * @param {function} middleware - ['isNotAuthorized']
  * @param {function} controller - ['resetPassword']
  * @returns {object} - router
  * @access public
  * @method GET
  */
-router.get('/reset-password', resetPassword);
+router.get('/reset-password', isNotAuthorized, resetPassword);
 
 /**
  * @description send verification email
- * @param {string} path - /auth/users/send-verification-email
+ * @param {string} path - /admin/send-verification-email
  * @param {function} controller - ['sendVerificationEmail']
  * @returns {object} - router
  * @access public
@@ -78,13 +92,23 @@ router.get('/send-verification-email', isAuthorized, sendVerificationEmail);
 
 /**
  * @description verify email
- * @param {string} path - /auth/users/verify-email
+ * @param {string} path - /admin/verify-email
  * @param {function} controller - ['verifyEmail']
  * @returns {object} - router
  * @access public
  * @method GET
  */
 router.get('/verify-email', verifyEmail);
+
+/**
+ * @description logout user
+ * @param {string} path - /admin/logout
+ * @param {function} controller - ['logout']
+ * @returns {object} - router
+ * @access public
+ * @method GET
+ */
+router.get('/logout', logout);
 
 // export
 module.exports = router;
