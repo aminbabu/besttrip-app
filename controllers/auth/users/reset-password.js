22d6a7b5f571c4dda52ed86547836a420f3ba930
Amin Babu <amin.babu.bd@gmail.com>
2024-05-27 08:16:54 +0600

    Update reset-password view controller to pass token to the reset-password view

--- controllers/auth/users/reset-password.js
+++ controllers/auth/users/reset-password.js
@@ -4,14 +4,19 @@
  * @version 0.0.0
  * @author best-trip
  * @date 22 May, 2024
- * @update_date 22 May, 2024
+ * @update_date 27 May, 2024
  */
 
 // export reset-password view controller
 module.exports = async (req, res, next) => {
     try {
+        // get token from query
+        const { token } = req.query;
+
         // render reset-password view
-        return res.render('auth/users/reset-password');
+        return res.render('auth/users/reset-password', {
+            token,
+        });
     } catch (error) {
         return next(error);
     }
