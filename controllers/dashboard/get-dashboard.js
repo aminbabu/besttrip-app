8b2e97104a535e6b426a6b0b775481032a5f531a
Amin Babu <amin.babu.bd@gmail.com>
2024-06-03 16:40:51 +0600

    Update redirect URL in catch block

--- controllers/dashboard/get-dashboard.js
+++ controllers/dashboard/get-dashboard.js
@@ -14,6 +14,6 @@ module.exports = (req, res) => {
             user: req.user,
         });
     } catch (error) {
-        return res.redirect('/500');
+        return res.redirect('/errors/500');
     }
 };
