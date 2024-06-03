bd3ab8dec6f427c072cacb8fd63e29c615f06988
Amin Babu <amin.babu.bd@gmail.com>
2024-06-03 17:13:38 +0600

    Update user routes and controllers

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,7 +3,7 @@
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 27 May, 2024
+ * @date 03 June, 2024
  * @update_date 03 June, 2024
  */
 
@@ -14,27 +14,10 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const {
-    getUsers,
-    getUser,
-    updateUser,
-    updateUserBySelf,
-    deleteUser,
-    deleteUserBySelf,
-} = require('../../controllers/users');
+const { getUsers, getUser } = require('../../controllers/users');
 
 // middlewares
-const { isAuthorized, isNotAllowed, isAllowed } = require('../../middlewares/auth');
-const {
-    validateUser,
-    validateUserSelf,
-    validateUserAccount,
-} = require('../../middlewares/validators/users');
-const { validateAvatar } = require('../../middlewares/validators/files');
-const { uploadAvatar } = require('../../middlewares/files');
-
-// constants
-const { USER_ROLES } = require('../../constants');
+const { isAuthorized, isNotAllowed } = require('../../middlewares/auth');
 
 /**
  * @description check if user is authorized
@@ -55,7 +38,7 @@ router.use(isAuthorized);
  * @access private - ['admin']
  * @method GET
  */
-router.get('/', isAllowed(['admin']), getUsers);
+router.get('/', isNotAllowed(['customer']), getUsers);
 
 /**
  * @description get user by mongo id
@@ -67,70 +50,3 @@ router.get('/', isAllowed(['admin']), getUsers);
  * @method GET
  */
 router.get('/:id', isNotAllowed(['customer']), getUser);
-
-/**
- * @description update user by self
- * @param {string} path - /dashboard/users/self
- * @param {function} middleware - ['isNotAllowed']
- * @param {function} validator - ['validateAvatar', 'validateUserAccount']
- * @param {function} validator - ['validateUserSelf']
- * @param {function} controller - ['updateUserBySelf']
- * @returns {object} - router
- * @access private - [USER_ROLES]
- * @method PATCH
- */
-router.patch(
-    '/self',
-    isNotAllowed(USER_ROLES),
-    validateAvatar,
-    validateUserAccount,
-    validateUserSelf,
-    uploadAvatar('avatars/users'),
-    updateUserBySelf
-);
-
-/**
- * @description update user by mongo id
- * @param {string} path - /dashboard/users/:id
- * @param {function} middleware - ['isNotAllowed']
- * @param {function} validator - ['validateAvatar', 'validateUserAccount']
- * @param {function} validator - ['validateUser']
- * @param {function} controller - ['updateUser']
- * @returns {object} - router
- * @access private - ['customer']
- * @method PATCH
- */
-router.patch(
-    '/:id',
-    isNotAllowed(['customer']),
-    validateAvatar,
-    validateUserAccount,
-    validateUser,
-    uploadAvatar('avatars/users'),
-    updateUser
-);
-
-/**
- * @description delete user by mongo id
- * @param {string} path - /dashboard/users/:id
- * @param {function} middleware - ['isNotAllowed']
- * @param {function} controller - ['deleteUser']
- * @returns {object} - router
- * @access private - ['customer']
- * @method DELETE
- */
-router.delete('/:id', isNotAllowed(['customer']), deleteUser);
-
-/**
- * @description delete user by self
- * @param {string} path - /dashboard/users/self
- * @param {function} middleware - ['isNotAllowed']
- * @param {function} controller - ['deleteUserBySelf']
- * @returns {object} - router
- * @access private - ['user']
- * @method DELETE
- */
-router.delete('/', isNotAllowed(['user']), deleteUserBySelf);
-
-// export
-module.exports = router;
