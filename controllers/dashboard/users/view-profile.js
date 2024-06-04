a607bf854044eda1d97f92c5ce788cade1c405a1
Amin Babu <amin.babu.bd@gmail.com>
2024-06-04 16:34:35 +0600

    Refactor profile view controller to format dates

--- controllers/dashboard/users/view-profile.js
+++ controllers/dashboard/users/view-profile.js
@@ -7,12 +7,26 @@
  * @update_date 04 June, 2024
  */
 
+// dependencies
+const moment = require('moment');
+
 // export profile view controller
 module.exports = (req, res) => {
     try {
+        const { user } = req;
+
+        // format dates
+        user.createdAt = moment(user.createdAt).format('DD MMM YYYY, h:mm a');
+        user.updatedAt = moment(user.updatedAt).format('DD MMM YYYY, h:mm a');
+
+        if (user?.lastLogin) {
+            user.lastLogin = moment(user.lastLogin).format('DD MMM YYYY, h:mm a');
+        }
+
+        // render profile view
         return res.render('dashboard/users/profile', {
             title: req.user.name,
-            user: req.user,
+            user,
         });
     } catch (error) {
         return res.redirect('/errors/500');
