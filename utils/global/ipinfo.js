cf3097c9554cbac1fd529af8450fa28ccc147193
Amin Babu <amin.babu.bd@gmail.com>
2024-06-04 19:25:47 +0600

    Refactor ipinfo function to improve readability and maintainability

--- utils/global/ipinfo.js
+++ utils/global/ipinfo.js
@@ -12,30 +12,26 @@ const moment = require('moment');
 const { History } = require('../../models');
 
 // export ipinfo function
-module.exports = async (req, res) => {
-    try {
-        // ip information
-        const { ip, city, region, country } = req.ipinfo || {};
+module.exports = async (req) => {
+    // ip information
+    const { ip, city, region, country } = req.ipinfo || {};
 
-        // create new history
-        const history = new History({
-            user: req.user._id,
-            lastLogin: moment().toDate(),
-            userAgent: req.headers['user-agent'],
-            ipAddress: ip,
-            location: {
-                city,
-                region,
-                country,
-            },
-        });
+    // create new history
+    const history = new History({
+        user: req.user._id,
+        lastLogin: moment().toDate(),
+        userAgent: req.headers['user-agent'],
+        ipAddress: ip,
+        location: {
+            city,
+            region,
+            country,
+        },
+    });
 
-        // save history
-        await history.save();
+    // save history
+    await history.save();
 
-        // return history
-        return history;
-    } catch (error) {
-        return res.redirect('/errors/500');
-    }
+    // return history
+    return history;
 };
