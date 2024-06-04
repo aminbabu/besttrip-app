7c9cc8718197c6e7a129b8c03117f36c4fcbc633
Amin Babu <amin.babu.bd@gmail.com>
2024-06-04 14:42:40 +0600

    Update auth validators date

--- middlewares/dashboard/validators/auth/index.js
+++ middlewares/dashboard/validators/auth/index.js
@@ -1,10 +1,10 @@
 /**
- * @file /middlewares/api/validators/auth/index.js
+ * @file /middlewares/dashboard/validators/auth/index.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 20 March, 2024
- * @update_date 14 April, 2024
+ * @date 04 June, 2024
+ * @update_date 04 June, 2024
  */
 
 // export auth validators middlewares
