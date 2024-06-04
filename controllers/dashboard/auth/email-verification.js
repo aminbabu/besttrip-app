41491564a498c4e386b89055a75120e70a994dfc
Amin Babu <amin.babu.bd@gmail.com>
2024-06-04 14:42:12 +0600

    Add email verification functionality

--- controllers/dashboard/auth/email-verification.js
+++ controllers/dashboard/auth/email-verification.js
@@ -7,13 +7,77 @@
  * @update_date 04 June, 2024
  */
 
+// dependencies
+const moment = require('moment');
+const { Token } = require('../../../models');
+const { User } = require('../../../models');
+const { confirmEmailVerification } = require('../../../mails');
+const { sendEmail } = require('../../../utils');
+
 // export verify-email view controller
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     try {
+        // get validated data
+        const { token } = req.query;
+
+        // get token
+        const emailVerificationToken = await Token.findOne({
+            token,
+            type: 'verify-email',
+        });
+
+        // check if token exists
+        if (!emailVerificationToken) {
+            return res.render('dashboard/auth/email-verification', {
+                title: 'Invalid or Expired Token',
+                message:
+                    'Provided token is invalid or expired. Please resend verification email to get a new token.',
+            });
+        }
+
+        // check if token is expired
+        if (moment(emailVerificationToken.expires).isBefore(moment())) {
+            return res.render('dashboard/auth/email-verification', {
+                title: 'Token Expired',
+                message:
+                    'Provided token is expired. Please resend verification email to get a new token.',
+            });
+        }
+
+        // get user
+        const user = await User.findById(emailVerificationToken.user);
+
+        // check if user exists
+        if (!user) {
+            return res.render('dashboard/auth/email-verification', {
+                title: 'User Not Found',
+                message: 'User not found. Please resend verification email to get a new token.',
+            });
+        }
+
+        // check if user is already verified
+        if (user.isVerified) {
+            return res.render('dashboard/auth/email-verification', {
+                title: 'User Already Verified',
+                message: 'User is already verified. Please login to your account.',
+            });
+        }
+
+        // update user
+        user.isVerified = true;
+        await user.save();
+
+        // delete token
+        await emailVerificationToken.deleteOne();
+
+        // send email
+        const info = await confirmEmailVerification(user.toObject());
+        await sendEmail(info.to, info.subject, info.text, info.html, info.attachments);
+
         return res.render('dashboard/auth/email-verification', {
-            title: 'Email Verification',
+            title: 'Email Verified',
             message:
-                'Your email has been verified successfully! You can now login to your account.',
+                'Email verified successfully. You can now login to your account and start using our services.',
         });
     } catch (err) {
         return res.redirect('/errors/500');
