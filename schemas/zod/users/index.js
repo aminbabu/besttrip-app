a6837e7e01868eca00851aad00c6d745bd92d520
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 23:08:28 +0600

    Update user schema and add update password schema

--- schemas/zod/users/index.js
+++ schemas/zod/users/index.js
@@ -4,10 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 April, 2024
- * @update_date 14 April, 2024
+ * @update_date 09 June, 2024
  */
 
 // export user zod schemas
 module.exports = {
     userSchema: require('./user'),
+    updatePasswordSchema: require('./update-password'),
 };
