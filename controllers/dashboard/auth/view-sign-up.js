bab65c3a68fe715e268b02e584ad17197d07ef9b
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 17:10:33 +0600

    Update sign-up view controller with countries list

--- controllers/dashboard/auth/view-sign-up.js
+++ controllers/dashboard/auth/view-sign-up.js
@@ -4,8 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 04 June, 2024
- * @update_date 04 June, 2024
+ * @update_date 09 June, 2024
  */
+
+// dependencies
+const { countries } = require('countries-list');
 
 // export sign-up view controller
 module.exports = (req, res) => {
@@ -14,6 +17,7 @@ module.exports = (req, res) => {
             title: 'Sign Up',
             message:
                 'Please enter your details to create an account. We will send you a verification email.',
+            countries: Object.values(countries),
         });
     } catch (err) {
         return res.redirect('/errors/500');
