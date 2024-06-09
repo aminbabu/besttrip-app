76a4945f536c93078ffbc99724fdae3a6e9e5cc5
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 18:37:46 +0600

    Add account activation email template and function

---
+++ mails/account-activation.js
@@ -0,0 +1,92 @@
+/**
+ * @file /mails/account-activation.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 09 June, 2024
+ * @update_date 09 June, 2024
+ */
+
+// dependencies
+const fs = require('fs');
+const ejs = require('ejs');
+const { env } = require('../config');
+
+// export send password reset confirmation mail
+module.exports = async (user) => {
+    let redirectTo;
+
+    // read template file
+    const template = fs.readFileSync(
+        `${__dirname}/../templates/email/account-activation.ejs`,
+        'utf-8'
+    );
+
+    // set redirect url
+    if (user.role === 'customer') {
+        redirectTo = `${env.APP_URL}:${env.PORT}/auth/customers/login`;
+    } else {
+        redirectTo = `${env.APP_URL}:${env.PORT}/dashboard/auth/login`;
+    }
+
+    // compile template
+    const html = ejs.render(template, {
+        company: {
+            name: env.APP_NAME,
+            address: '123, Best Trip Street, Best Trip City',
+            phone: '+1234567890',
+            email: env.EMAIL_FROM,
+            website: `${env.APP_URL}:${env.PORT}`,
+        },
+        user,
+        redirectTo,
+    });
+
+    // attachments
+    const attachments = [
+        {
+            filename: 'favicon.ico',
+            path: `${__dirname}/../public/assets/media/logos/favicon.ico`,
+            cid: 'favicon',
+        },
+        {
+            filename: 'logo-1.svg',
+            path: `${__dirname}/../public/assets/media/email/logo-1.svg`,
+            cid: 'logo',
+        },
+        {
+            filename: 'icon-positive-vote-1.svg',
+            path: `${__dirname}/../public/assets/media/email/icon-positive-vote-1.svg`,
+            cid: 'positiveVote',
+        },
+        {
+            filename: 'icon-linkedin.svg',
+            path: `${__dirname}/../public/assets/media/email/icon-linkedin.svg`,
+            cid: 'linkedin',
+        },
+        {
+            filename: 'icon-dribbble.svg',
+            path: `${__dirname}/../public/assets/media/email/icon-dribbble.svg`,
+            cid: 'dribbble',
+        },
+        {
+            filename: 'icon-facebook.svg',
+            path: `${__dirname}/../public/assets/media/email/icon-facebook.svg`,
+            cid: 'facebook',
+        },
+        {
+            filename: 'icon-twitter.svg',
+            path: `${__dirname}/../public/assets/media/email/icon-twitter.svg`,
+            cid: 'twitter',
+        },
+    ];
+
+    // return mail options
+    return {
+        to: user?.email,
+        subject: 'Account Activation',
+        text: 'Successfully your account has been activated. You can now login to your account',
+        html,
+        attachments,
+    };
+};
