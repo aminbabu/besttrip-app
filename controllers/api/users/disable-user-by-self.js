23c8cc7e6a91f7e200550279869a7fe6499d45dc
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 21:00:07 +0600

    Add disable-user-by-self controller

---
+++ controllers/api/users/disable-user-by-self.js
@@ -0,0 +1,39 @@
+/**
+ * @file /controllers/api/users/disable-user-by-self.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 09 June, 2024
+ * @update_date 09 June, 2024
+ */
+
+// dependencies
+const { User } = require('../../../models');
+
+// export disable user by self controller
+module.exports = async (req, res) => {
+    try {
+        // get user id
+        const { _id } = req.user;
+
+        // find user
+        const user = await User.findById(_id);
+
+        // check if user exists
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // set user status to disabled
+        user.set({ status: 'disabled' });
+
+        // save user
+        await user.save();
+
+        // return response
+        return res.json({ message: 'Disabled user successfully' });
+    } catch (error) {
+        // return error response
+        return res.status(500).json({ message: error.message });
+    }
+};
