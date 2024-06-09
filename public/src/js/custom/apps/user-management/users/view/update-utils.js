3fb146848f3a0ad21b20bda0616dfe6b345fd0ed
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 21:13:07 +0600

    Add user account deletion functionality

--- public/src/js/custom/apps/user-management/users/view/update-utils.js
+++ public/src/js/custom/apps/user-management/users/view/update-utils.js
@@ -76,10 +76,82 @@ const KTUsersUpdateUtils = (function () {
         });
     };
 
+    // Delete user account
+    const initDeleteAccount = () => {
+        if (!userDeleteBtn) {
+            return;
+        }
+
+        userDeleteBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            // Show confirm popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+            Swal.fire({
+                text: 'Are you sure you want to delete this user account?',
+                icon: 'warning',
+                buttonsStyling: false,
+                showCancelButton: true,
+                reverseButtons: true,
+                cancelButtonText: 'No, cancel',
+                confirmButtonText: 'Yes, delete!',
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-light',
+                },
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    axios
+                        .delete(userDeleteBtn.getAttribute('href'))
+                        .then((response) => {
+                            // Show success message. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                            Swal.fire({
+                                text: 'User account has been deleted. Redirecting to the login page...',
+                                icon: 'success',
+                                buttonsStyling: false,
+                                confirmButtonText: 'Ok, got it!',
+                                customClass: {
+                                    confirmButton: 'btn btn-primary',
+                                },
+                                allowOutsideClick: false,
+                                showCancelButton: false,
+                            }).then((outcome) => {
+                                if (outcome.isConfirmed) {
+                                    location.href = '/dashboard/login';
+                                }
+                            });
+                        })
+                        .catch((error) => {
+                            const errors = error.response.data.message
+                                ? error.response.data.message
+                                : error.response.data.errors;
+
+                            Swal.fire({
+                                html: `${
+                                    errors instanceof Array
+                                        ? `<ul class="text-start
+                                        ">${Object.values(error.response.data.errors)
+                                            .map((err) => `<li>${err?.message}</li>`)
+                                            .join('')}</ul>`
+                                        : errors
+                                }`,
+                                icon: 'error',
+                                buttonsStyling: false,
+                                confirmButtonText: 'Ok, got it!',
+                                customClass: {
+                                    confirmButton: 'btn btn-primary',
+                                },
+                            });
+                        });
+                }
+            });
+        });
+    };
+
     return {
         // Public functions
         init() {
             initUpdateStatus();
+            initDeleteAccount();
         },
     };
 }());
