f0c946159c0a2a026dfffad7edcc6d5d4676753c
Amin Babu <amin.babu.bd@gmail.com>
2024-06-09 23:08:22 +0600

    Add update password schema

---
+++ schemas/zod/users/update-password.js
@@ -0,0 +1,33 @@
+/**
+ * @file /schemas/zod/users/update-password.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 09 June, 2024
+ * @update_date 09 June, 2024
+ */
+
+// dependencies
+const { z } = require('zod');
+
+// export update password schema
+module.exports = z
+    .object({
+        password: z
+            .string({
+                required_error: 'Password is required',
+                invalid_type_error: 'Password must be a string',
+            })
+            .min(8, 'Password must be at least 8 characters long.'),
+        confirmPassword: z
+            .string({
+                required_error: 'Confirm password is required',
+                invalid_type_error: 'Confirm password must be a string',
+            })
+            .min(8, 'Confirm password must be at least 8 characters long.')
+            .refine(({ password, confirmPassword }) => password === confirmPassword, {
+                message: 'Passwords do not match',
+                path: ['confirmPassword'],
+            }),
+    })
+    .strict();
