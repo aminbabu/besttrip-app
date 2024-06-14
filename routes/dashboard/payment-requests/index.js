52fbde558fffcf4b01b6a0d97d0503b0d095820e
Amin Babu <amin.babu.bd@gmail.com>
2024-06-14 16:41:21 +0600

    Add payment requests route to dashboard

---
+++ routes/dashboard/payment-requests/index.js
@@ -0,0 +1,42 @@
+/**
+ * @file routes/dashboard/payment-requests/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// dependencies
+const express = require('express');
+
+// express router
+const router = express.Router();
+
+// controllers
+const { viewPaymentRequests } = require('../../../controllers/dashboard/payment-requests');
+
+// middlewares
+const { isAuthorized } = require('../../../middlewares/dashboard/auth');
+
+/**
+ * @description check if user is authorized
+ * @param {string} path - '/dashboard/payment-requests'
+ * @param {function} middleware - ['isAuthorized']
+ * @returns {object} - router
+ * @method USE
+ */
+router.use(isAuthorized);
+
+/**
+ * @description - profile view route
+ * @param {string} path - '/dashboard/payment-requests/'
+ * @param {function} controller - ['viewProfile']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/', viewPaymentRequests);
+
+// export profile router
+module.exports = router;
