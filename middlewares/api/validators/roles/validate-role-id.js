a4f701c54495f26b4ed716a77c054c71d81f2ced
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 01:11:54 +0600

    Add role ID validation middleware

---
+++ middlewares/api/validators/roles/validate-role-id.js
@@ -0,0 +1,27 @@
+/**
+ * @file /middlewares/api/validators/roles/validate-role-id.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// dependencies
+const { roleSchema } = require('../../../../schemas/zod/roles');
+const { zodErrorHandler } = require('../../../../handlers/errors');
+
+// export payment request id validator middleware
+module.exports = (req, res, next) => {
+    // validate request body
+    const { error, success } = roleSchema.pick({ id: true }).safeParse(req.params);
+
+    // check for errors
+    if (!success) {
+        // return error response
+        return zodErrorHandler(res, error);
+    }
+
+    // proceed to next middleware
+    return next();
+};
