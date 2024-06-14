932b6a17e85576ba458a4a178b6b70bf0b59074e
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 01:11:30 +0600

    Add role schema export

---
+++ schemas/zod/roles/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file schemas/zod/roles/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// export role schema
+module.exports = {
+    roleSchema: require('./role'),
+};
