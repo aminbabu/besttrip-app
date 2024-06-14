e02a094fd6496e0d37554af78b99ee9360f37cc0
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 01:47:02 +0600

    Add customers routes to dashboard

---
+++ routes/dashboard/customers/index.js
@@ -0,0 +1,52 @@
+/**
+ * @file routes/dashboard/customers/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// dependencies
+const express = require('express');
+
+// express router
+const router = express.Router();
+
+// controllers
+const { viewCustomers, viewCustomer } = require('../../../controllers/dashboard/customers');
+
+// middlewares
+const { isAuthorized } = require('../../../middlewares/dashboard/auth');
+
+/**
+ * @description check if customer is authorized
+ * @param {string} path - '/dashboard/customers'
+ * @param {function} middleware - ['isAuthorized']
+ * @returns {object} - router
+ * @method USE
+ */
+router.use(isAuthorized);
+
+/**
+ * @description - customers view route
+ * @param {string} path - '/dashboard/customers'
+ * @param {function} controller - ['viewCustomers']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/', viewCustomers);
+
+/**
+ * @description - view customer view route
+ * @param {string} path - '/dashboard/customers/view'
+ * @param {function} controller - ['viewCustomer']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/:id', viewCustomer);
+
+// export profile router
+module.exports = router;
