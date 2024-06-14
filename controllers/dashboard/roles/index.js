1ddeba93b75c0924f8957c263a8a3009dc113a90
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 01:18:56 +0600

    Add role controllers to dashboard

---
+++ controllers/dashboard/roles/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /controllers/dashboard/roles/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// export all role controllers
+module.exports = {
+    viewRoles: require('./view-roles'),
+};
