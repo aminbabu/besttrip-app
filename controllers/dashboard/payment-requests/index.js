977534c60547bc5f928313fd60c3aae2bfc2b62c
Amin Babu <amin.babu.bd@gmail.com>
2024-06-14 16:41:13 +0600

    Add payment requests controllers

---
+++ controllers/dashboard/payment-requests/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file controllers/dashboard/payment-requests/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// export payment requests controllers
+module.exports = {
+    viewPaymentRequests: require('./view-payment-requests'),
+};
