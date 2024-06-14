8e2548101298325a381572472ddfcde8284d7084
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 00:18:16 +0600

    Add get-payment-requests-by-status controller

---
+++ controllers/api/payment-requests/get-payment-requests-by-status.js
@@ -0,0 +1,30 @@
+/**
+ * @file /controllers/api/payment-requests/get-payment-requests-by-status.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 14 June, 2024
+ * @update_date 14 June, 2024
+ */
+
+// dependencies
+const { PaymentRequest } = require('../../../models');
+
+// export get payment requests by status controller
+module.exports = async (req, res, next) => {
+    try {
+        // get status from request
+        const { status } = req.params;
+
+        // get payment requests
+        const paymentRequests = await PaymentRequest.find({ status }).populate('customer');
+
+        // return response
+        return res.status(200).json({
+            message: 'Fetched payment requests by status successfully',
+            paymentRequests,
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
