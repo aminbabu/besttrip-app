4ee5a3f91aa0215d5bd8f4d26dbc55139b9f6dc7
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 01:40:10 +0600

    Add edit user view route

--- routes/dashboard/users/index.js
+++ routes/dashboard/users/index.js
@@ -14,7 +14,7 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { viewUsers, viewProfile } = require('../../../controllers/dashboard/users');
+const { viewUsers, editUser, viewProfile } = require('../../../controllers/dashboard/users');
 
 // middlewares
 const { isAuthorized } = require('../../../middlewares/dashboard/auth');
@@ -47,6 +47,16 @@ router.get('/', viewUsers);
  * @method GET
  */
 router.get('/profile', viewProfile);
+
+/**
+ * @description - edit user view route
+ * @param {string} path - '/dashboard/users/edit'
+ * @param {function} controller - ['editUser']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/edit', editUser);
 
 // export profile router
 module.exports = router;
