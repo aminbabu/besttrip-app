dc46078ae78dd2de3149829d58f8bd8520e1bd6a
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:44:09 +0600

    Add invoice view controller

---
+++ controllers/dashboard/invoices/view-invoices.js
@@ -0,0 +1,28 @@
+/**
+ * @file controllers/dashboard/invoices/view-invoices.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const { Invoice } = require('../../../models');
+
+// export invoice view controller
+module.exports = async (req, res) => {
+    try {
+        // get invoice
+        const invoices = await Invoice.find();
+
+        // return render view
+        return res.render('dashboard/invoices', {
+            title: 'Invoices',
+            user: req.user,
+            invoices,
+        });
+    } catch (error) {
+        return res.redirect('/error/500');
+    }
+};
