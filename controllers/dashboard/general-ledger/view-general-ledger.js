d0e686fff415b96af91295c09ce4a552f54f8ae4
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 13:19:13 +0600

    Add general ledger view controller

---
+++ controllers/dashboard/general-ledger/view-general-ledger.js
@@ -0,0 +1,28 @@
+/**
+ * @file controllers/dashboard/general-ledger/view-general-ledger.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const { GeneralLedger } = require('../../../models');
+
+// export general ledger view controller
+module.exports = async (req, res) => {
+    try {
+        // get general ledger
+        const generalLedger = await GeneralLedger.find();
+
+        // return render view
+        return res.render('dashboard/general-ledger', {
+            title: 'General Ledger',
+            user: req.user,
+            generalLedger,
+        });
+    } catch (error) {
+        return res.redirect('/error/500');
+    }
+};
