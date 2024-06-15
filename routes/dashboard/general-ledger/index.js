24f760ec308573c2387e80c684d5ff832934eb10
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 13:19:30 +0600

    Add general ledger route to dashboard

---
+++ routes/dashboard/general-ledger/index.js
@@ -0,0 +1,42 @@
+/**
+ * @file routes/dashboard/general-ledger/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const express = require('express');
+
+// express router
+const router = express.Router();
+
+// controllers
+const { viewGeneralLedger } = require('../../../controllers/dashboard/general-ledger');
+
+// middlewares
+const { isAuthorized } = require('../../../middlewares/dashboard/auth');
+
+/**
+ * @description check if user is authorized
+ * @param {string} path - '/dashboard/general-ledger'
+ * @param {function} middleware - ['isAuthorized']
+ * @returns {object} - router
+ * @method USE
+ */
+router.use(isAuthorized);
+
+/**
+ * @description - umrah bookings view route
+ * @param {string} path - '/dashboard/general-ledgers'
+ * @param {function} controller - ['viewGeneralLedger']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/', viewGeneralLedger);
+
+// export router
+module.exports = router;
