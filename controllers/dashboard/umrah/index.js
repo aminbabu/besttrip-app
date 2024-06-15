ec56235d813cecdbfc9201b6026ec072091ea333
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 11:19:00 +0600

    Add new umrah controllers

--- controllers/dashboard/umrah/index.js
+++ controllers/dashboard/umrah/index.js
@@ -10,4 +10,9 @@
 // export all umrah controllers
 module.exports = {
     viewUmrahBooking: require('./view-umrah-booking'),
+    viewUmrahTypes: require('./view-umrah-types'),
+    viewUmrahDurations: require('./view-umrah-durations'),
+    viewUmrahPackages: require('./view-umrah-packages'),
+    viewAddUmrahPackage: require('./view-add-umrah-package'),
+    viewEditUmrahPackage: require('./view-edit-umrah-package'),
 };
