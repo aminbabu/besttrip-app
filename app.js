676fbb5f3690415631b3edf302e58973b555312e
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 14:26:38 +0600

    Add error logging in app.js

--- app.js
+++ app.js
@@ -72,6 +72,8 @@ app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+    console.log(err);
+
     // send error response
     return res.status(err.status || 500).json({
         message: err.message,
