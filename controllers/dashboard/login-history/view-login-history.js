ecccf1aa3b5be311004bf18ca879bb363097d0d1
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 13:34:40 +0600

    Add login history view controller

---
+++ controllers/dashboard/login-history/view-login-history.js
@@ -0,0 +1,28 @@
+/**
+ * @file controllers/dashboard/lgoin-history/view-lgoin-history.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const { History } = require('../../../models');
+
+// export login history view controller
+module.exports = async (req, res) => {
+    try {
+        // get login history
+        const loginHistory = await History.find().sort({ createdAt: -1 });
+
+        // render login history view
+        return res.render('dashboard/login-history', {
+            title: 'Login History',
+            user: req.user,
+            loginHistory,
+        });
+    } catch (error) {
+        return res.redirect('/error/500');
+    }
+};
