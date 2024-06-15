9c94091d274ccca3f7d3f54e50e4f0c1d2aecbb2
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:03:13 +0600

    Add umrah bookings status constants

---
+++ constants/umrah-bookings.js
@@ -0,0 +1,16 @@
+/**
+ * @file /constants/umrah-bookings.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// umrah bookings status
+const UMRAH_BOOKING_STATUS = ['in-process', 'under-review', 'success', 'blocked', 'cancelled'];
+
+// export umrah bookings status
+module.exports = {
+    UMRAH_BOOKING_STATUS,
+};
