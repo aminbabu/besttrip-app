8348a5605fb6739525477f07e5ea062bc6522853
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:17:28 +0600

    Add route for viewing umrah bookings in the dashboard

---
+++ routes/dashboard/umrah-booking/index.js
@@ -0,0 +1,42 @@
+/**
+ * @file routes/dashboard/umrah-booking/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const express = require('express');
+
+// express router
+const router = express.Router();
+
+// controllers
+const { viewUmrahBooking } = require('../../../controllers/dashboard/umrah/booking');
+
+// middlewares
+const { isAuthorized } = require('../../../middlewares/dashboard/auth');
+
+/**
+ * @description check if user is authorized
+ * @param {string} path - '/dashboard/payment-requests'
+ * @param {function} middleware - ['isAuthorized']
+ * @returns {object} - router
+ * @method USE
+ */
+router.use(isAuthorized);
+
+/**
+ * @description - umrah bookings view route
+ * @param {string} path - '/dashboard/umrah-booking/:status'
+ * @param {function} controller - ['viewUmrahBooking']
+ * @returns {object} - router
+ * @access private - ['all']
+ * @method GET
+ */
+router.get('/:status', viewUmrahBooking);
+
+// export router
+module.exports = router;
