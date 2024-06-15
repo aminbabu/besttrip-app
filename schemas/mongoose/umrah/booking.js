58e4a8db6c2287216bcaba14d824cc51181dc2c9
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:07:50 +0600

    Add mongoose schema for umrah bookings

---
+++ schemas/mongoose/umrah/booking.js
@@ -0,0 +1,36 @@
+/**
+ * @file /schemas/mongoose/umrah/bookings.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const { Schema } = require('mongoose');
+const { UMRAH_BOOKING_STATUS } = require('../../../constants');
+
+// export umrah package type schema
+module.exports = new Schema(
+    {
+        customer: {
+            type: Schema.Types.ObjectId,
+            ref: 'Customer',
+            required: true,
+        },
+        umrahPackage: {
+            type: Schema.Types.ObjectId,
+            ref: 'UmrahPackage',
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: UMRAH_BOOKING_STATUS,
+            default: UMRAH_BOOKING_STATUS[0],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
