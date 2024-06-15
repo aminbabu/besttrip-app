d4551f3065a5ea85dd639ab3e7c574bc8db6d954
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:17:12 +0600

    Add umrah booking view controller

---
+++ controllers/dashboard/umrah/booking/view-umrah-booking.js
@@ -0,0 +1,31 @@
+/**
+ * @file controllers/dashboard/umrah/booking/view-umrah-booking.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// dependencies
+const { UmrahBooking } = require('../../../../models');
+
+// export umrah booking view controller
+module.exports = async (req, res) => {
+    try {
+        // get status from request params
+        const { status } = req.params;
+
+        // get umrah bookings
+        const umrahBookings = await UmrahBooking.find({ status }).populate('customer');
+
+        // return render view
+        return res.render(`dashboard/umrah/${status}`, {
+            title: 'Umrah Bookings',
+            user: req.user,
+            umrahBookings,
+        });
+    } catch (error) {
+        return res.redirect('/error/500');
+    }
+};
