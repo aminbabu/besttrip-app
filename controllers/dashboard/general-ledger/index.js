4e5309b16f8087603d30fd82a746ee9b7a885c00
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 13:19:20 +0600

    Add general ledger controllers

---
+++ controllers/dashboard/general-ledger/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file controllers/dashboard/general-ledger/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// export general ledger controllers
+module.exports = {
+    viewPaymentRequests: require('./view-general-ledger'),
+};
