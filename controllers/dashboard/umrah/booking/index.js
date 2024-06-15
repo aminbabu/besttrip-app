4bd7eaa40fe82271741a2918a596880ddcd0bf03
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 10:17:19 +0600

    Add umrah booking controllers

---
+++ controllers/dashboard/umrah/booking/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file controllers/dashboard/umrah/booking/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// export all umrah booking controllers
+module.exports = {
+    viewUmrahBooking: require('./view-umrah-booking'),
+};
