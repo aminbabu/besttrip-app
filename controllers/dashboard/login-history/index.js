6240733bac05ea871423da9dfc89d6cf869c0ba3
Amin Babu <amin.babu.bd@gmail.com>
2024-06-15 13:34:43 +0600

    Add login history controller

---
+++ controllers/dashboard/login-history/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file controllers/dashboard/login-history/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 15 June, 2024
+ * @update_date 15 June, 2024
+ */
+
+// export login history controller
+module.exports = {
+    viewLoginHistory: require('./view-login-history'),
+};
