0a7109ce93da68c398f9c382b78d6aa70f8437e4
Amin Babu <amin.babu.bd@gmail.com>
2024-06-16 15:11:31 +0600

    Refactor currency formatter in currency-formatter.js

--- utils/global/currency-formatter.js
+++ utils/global/currency-formatter.js
@@ -13,13 +13,9 @@ const currency = require('currency.js');
 // export currency formatter function
 module.exports = (price) =>
     currency(price, {
-        precision: 2, // Set the number of decimal places
-        formatWith: {
-            symbol: ' BDT', // Add a space before "BDT" for better readability
-            thousand: ',',
-            decimal: '.',
-            prefix: '',
-            suffix: '',
-            format: '{symbol}{prefix}{number}{fractional}', // Customize the format string
-        },
-    }).format(); // Outputs: 1,234.56 BDT
+        symbol: 'BDT',
+        separator: ',',
+        decimal: '.',
+        precision: 2,
+        pattern: '# !',
+    }).format();
