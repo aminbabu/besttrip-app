ed831405977bffef6e88f41ea93c1e671131a012
Amin Babu <amin.babu.bd@gmail.com>
2024-06-16 13:13:23 +0600

    Update payment requests validators

--- middlewares/api/validators/payment-requests/index.js
+++ middlewares/api/validators/payment-requests/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 20 April, 2024
- * @update_date 14 June, 2024
+ * @update_date 16 June, 2024
  */
 
 // export payment requests validator middlewares
@@ -12,4 +12,5 @@ module.exports = {
     validatePaymentRequestId: require('./validate-payment-request-id'),
     validatePaymentRequest: require('./validate-payment-request'),
     validatePaymentRequestsByStatus: require('./validate-payment-requests-by-status'),
+    validatePaymentRequestAttachment: require('./validate-payment-request-attachment'),
 };
