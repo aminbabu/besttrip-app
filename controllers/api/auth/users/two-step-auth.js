69018b9789938f15c71de0f58bc853bea6e9c435
Amin Babu <amin.babu.bd@gmail.com>
2024-06-16 11:23:31 +0600

    Add two-step authentication controller

---
+++ controllers/api/auth/users/two-step-auth.js
@@ -0,0 +1,55 @@
+/**
+ * @file /controllers/api/auth/users/two-step-auth.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 16 June, 2024
+ * @update_date 16 June, 2024
+ */
+
+// dependencies
+const { User } = require('../../../../models');
+const { comparePassword } = require('../../../../utils');
+
+// export two step auth controller
+module.exports = async (req, res, next) => {
+    try {
+        // get validated data
+        const { email, password } = req.body;
+
+        // check if user exists
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Please check your email and password',
+            });
+        }
+
+        // convert user to object
+        const userObject = user.toObject();
+
+        // compare password
+        const match = await comparePassword(password, userObject.password);
+
+        // check if password match
+        if (!match) {
+            return res.status(400).json({
+                message: 'Please check your email and password',
+            });
+        }
+
+        // update user two step auth
+        user.set({ twoStepAuth: true });
+
+        // save user
+        await user.save();
+
+        // return response
+        return res.status(200).json({
+            message: 'Two step authentication is enabled',
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
