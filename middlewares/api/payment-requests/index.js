03a4aa1f846961ea8e01395b3a50aceaa6152287
Amin Babu <amin.babu.bd@gmail.com>
2024-06-16 13:07:06 +0600

    Add payment requests middlewares

---
+++ middlewares/api/payment-requests/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file middlewares/api/payment-requests/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 16 June, 2024
+ * @update_date 16 June, 2024
+ */
+
+// export payment requests middlewares
+module.exports = {
+    uploadPaymentAttachment: require('./upload-payment-attachment'),
+};
