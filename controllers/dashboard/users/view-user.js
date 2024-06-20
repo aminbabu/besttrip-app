e0918f7cffa3e6dbb9209119731a90bd8dcb0a43
Amin Babu <amin.babu.bd@gmail.com>
2024-06-20 14:47:00 +0600

    Refactor user view controller to display profile information

--- controllers/dashboard/users/view-user.js
+++ controllers/dashboard/users/view-user.js
@@ -1,35 +1,54 @@
 /**
- * @file controllers/dashboard/users/edit-user.js
+ * @file /controllers/dashboard/users/view-profile.js
  * @project best-trip
  * @version 0.0.0
  * @author best-trip
- * @date 14 June, 2024
- * @update_date 14 June, 2024
+ * @date 04 June, 2024
+ * @update_date 20 June, 2024
  */
 
 // dependencies
+const moment = require('moment');
+const { countries } = require('countries-list');
 const { User } = require('../../../models');
 
-// export user view controller
+// export profile view controller
 module.exports = async (req, res) => {
     try {
-        // get user id from request params
+        // validated data
         const { id } = req.params;
 
-        // get user
-        const user = await User.findById(id);
+        // get user with histories
+        const existingUser = await User.findById(id).populate('histories');
 
-        // check if user exists
-        if (!user) {
-            return res.redirect('/error/404');
-        }
+        // convert user and login histories to object
+        const user = existingUser.toObject();
 
-        // return rendered view
+        // format user dates
+        user.createdAt = moment(user.createdAt).format('DD MMM YYYY, h:mm a');
+        user.updatedAt = moment(user.updatedAt).format('DD MMM YYYY, h:mm a');
+        user.histories = user.histories.map((history) => {
+            const lastLogin = moment(history?.lastLogin).format('DD MMM YYYY, h:mm a');
+            const lastLoginDaysAgo = moment(history?.lastLogin).fromNow();
+            const location = history?.location
+                ? Object.values(history.location).filter(Boolean).join(', ')
+                : 'N/A';
+
+            return {
+                ...history,
+                lastLogin,
+                lastLoginDaysAgo,
+                location,
+            };
+        });
+
+        // render profile view
         return res.render('dashboard/users/user', {
-            title: 'Edit User',
+            title: req.user.name,
             user,
+            countries: Object.values(countries),
         });
     } catch (error) {
-        return res.redirect('/error/500');
+        return res.redirect('/errors/500');
     }
 };
