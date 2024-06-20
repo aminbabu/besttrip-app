25bb8d53bea756d9bb53ea577419f1d7b20beeb9
Amin Babu <amin.babu.bd@gmail.com>
2024-06-20 14:47:15 +0600

    Update user controller import

--- controllers/dashboard/users/index.js
+++ controllers/dashboard/users/index.js
@@ -4,12 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 04 June, 2024
- * @update_date 14 June, 2024
+ * @update_date 20 June, 2024
  */
 
 // export all user controllers
 module.exports = {
     viewUsers: require('./view-users'),
     viewUser: require('./view-user'),
-    viewProfile: require('./view-profile'),
 };
