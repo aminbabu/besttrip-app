2d36950a20af2cbbdebeb2e306e704c231736f14
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 11:15:21 +0600

    Update initial user registering logic

--- controllers/dashboard/auth/view-sign-up.js
+++ controllers/dashboard/auth/view-sign-up.js
@@ -4,15 +4,25 @@
  * @version 0.0.0
  * @author best-trip
  * @date 04 June, 2024
- * @update_date 09 June, 2024
+ * @update_date 21 June, 2024
  */
 
 // dependencies
 const { countries } = require('countries-list');
+const { User } = require('../../../models');
 
 // export sign-up view controller
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     try {
+        // find at least an admin user
+        const user = await User.findOne({ role: 'admin' });
+
+        // check if user is not exists
+        if (user) {
+            return res.redirect('/dashboard/auth/login');
+        }
+
+        // return render sign up view
         return res.render('dashboard/auth/sign-up', {
             title: 'Sign Up',
             message:
