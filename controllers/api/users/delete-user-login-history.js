40edcf38c455b9ca30db03fdd1aa92a8b9821430
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 15:38:31 +0600

    Add delete user login history controller

---
+++ controllers/api/users/delete-user-login-history.js
@@ -0,0 +1,41 @@
+/**
+ * @file /controllers/api/users/delete-user-login-history.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 21 June, 2024
+ * @update_date 21 June, 2024
+ */
+
+// dependencies
+const { User, LoginHistory } = require('../../../models');
+
+// export delete user login history controller
+module.exports = async (req, res, next) => {
+    try {
+        // get user id
+        const { id } = req.params;
+
+        // get user login history
+        const loginHistory = await LoginHistory.find({ user: id });
+
+        // delete user login history
+        const user = await User.findById(id);
+
+        // update user
+        user.set({ loginHistory: [] });
+
+        // delete user login history
+        await loginHistory.deleteMany();
+
+        // save user
+        await user.save();
+
+        // return response
+        return res.status(200).json({
+            message: 'Deleted user login history successfully',
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
