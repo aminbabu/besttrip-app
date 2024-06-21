86bdb67c0e0c77876e6f62b8ebfe7f0ec6d7fabb
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 21:52:20 +0600

    Update sign-out controller to delete login history

--- controllers/dashboard/home/sign-out.js
+++ controllers/dashboard/home/sign-out.js
@@ -4,12 +4,39 @@
  * @version 0.0.0
  * @author best-trip
  * @date 04 June, 2024
- * @update_date 04 June, 2024
+ * @update_date 21 June, 2024
  */
 
+// dependencies
+const { verifyToken } = require('../../../utils');
+const { LoginHistory } = require('../../../models');
+
 // export sign-out controller
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     try {
+        // get token from cookie or header
+        const token = req.cookies.token || req.headers.authorization;
+
+        // ip information
+        const { ip } = req.ipinfo;
+
+        // verify token
+        const { user } = verifyToken(token) || {};
+
+        // get last history
+        const history = await LoginHistory.findOne({
+            user: user?._id,
+            ipAddress: ip,
+            userAgent: req.headers['user-agent'],
+        }).sort({
+            createdAt: -1,
+        });
+
+        // delete history
+        if (history) {
+            history.deleteOne();
+        }
+
         // clear cookie and header
         res.clearCookie('token');
         res.removeHeader('Authorization');
