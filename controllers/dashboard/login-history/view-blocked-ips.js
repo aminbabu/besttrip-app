fdaf82416a249ae5f13439056489276633e68d49
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 10:58:44 +0600

    Fix login history model name

--- controllers/dashboard/login-history/view-blocked-ips.js
+++ controllers/dashboard/login-history/view-blocked-ips.js
@@ -4,17 +4,17 @@
  * @version 0.0.0
  * @author best-trip
  * @date 15 June, 2024
- * @update_date 15 June, 2024
+ * @update_date 21 June, 2024
  */
 
 // dependencies
-const { History } = require('../../../models');
+const { LoginHistory } = require('../../../models');
 
 // export blocked ips view controller
 module.exports = async (req, res) => {
     try {
         // get blocked ips
-        const blockedIps = await History.find({ status: 'blocked' }).sort({ createdAt: -1 });
+        const blockedIps = await LoginHistory.find({ status: 'blocked' }).sort({ createdAt: -1 });
 
         // render blocked ips view
         return res.render('dashboard/login-history/blocked-ips', {
