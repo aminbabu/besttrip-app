9b7b91ce11fcf340421506eafe49b33ef68b393a
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 23:33:54 +0600

    Add update role controller

---
+++ controllers/api/users/update-role.js
@@ -0,0 +1,42 @@
+/**
+ * @file /controllers/api/users/update-role.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 21 June, 2024
+ * @update_date 21 June, 2024
+ */
+
+const { User } = require('../../../models');
+
+// export update role controller
+module.exports = async (req, res, next) => {
+    try {
+        // get validated data
+        const { role } = req.body;
+        const { id } = req.params;
+
+        // get user
+        const user = await User.findById(id);
+
+        // check if user exists
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+
+        // set user role
+        user.set({ role });
+
+        // save user
+        await user.save();
+
+        // return response
+        return res.status(200).json({
+            message: 'User role updated successfully',
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
