43f3a7c61f230fdcefff921a99942ba2cc3111a6
Amin Babu <amin.babu.bd@gmail.com>
2024-06-21 23:31:59 +0600

    Update user validators with role validation

--- middlewares/api/validators/users/index.js
+++ middlewares/api/validators/users/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 08 April, 2024
- * @update_date 09 June, 2024
+ * @update_date 21 June, 2024
  */
 
 // export user validators
@@ -14,4 +14,5 @@ module.exports = {
     validatePassword: require('./validate-password'),
     validateUserSelf: require('./validate-user-self'),
     validatePasswordSelf: require('./validate-password-self'),
+    validateRole: require('./validate-role'),
 };
