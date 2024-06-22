dfecdfb200ce0842e71b384b55caf0d7ce1070cc
Amin Babu <amin.babu.bd@gmail.com>
2024-06-22 18:47:39 +0600

    Add enable user controller

---
+++ controllers/api/users/enable-user.js
@@ -0,0 +1,38 @@
+/**
+ * @file /controllers/api/users/enable-user.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 22 June, 2024
+ * @update_date 22 June, 2024
+ */
+
+// dependencies
+const { User } = require("../../../models");
+
+// export enable user controller
+module.exports = async (req, res, next) => {
+  try {
+    // get user id
+    const { id } = req.params;
+
+    // find user
+    const user = await User.findById(id);
+
+    // check if user exists
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // set user status to active
+    user.set({ status: "active" });
+
+    // save user
+    await user.save();
+
+    // return response
+    return res.json({ message: "Enabled user successfully" });
+  } catch (error) {
+    return next(error);
+  }
+};
