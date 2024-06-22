696d07589d290ad250514e2f7e56fc4eba3de77b
Amin Babu <amin.babu.bd@gmail.com>
2024-06-22 19:58:41 +0600

    Add user login history functionality

---
+++ public/src/js/custom/apps/user-management/users/view/update-login-history.js
@@ -0,0 +1,173 @@
+// Class definition
+const KTUsersUpdateLoginHistory = (function () {
+  // Shared variables
+  const loginHistoryBlockBtns = document.querySelectorAll(
+    '[data-kt-users-login-session-table-filter="block"]'
+  );
+  const loginHistorySignOutBtns = document.querySelectorAll(
+    '[data-kt-users-login-session-table-filter="signout"]'
+  );
+
+  // Block login session
+  const handleLoginHistoryStatus = () => {
+    if (!loginHistoryBlockBtns) {
+      return;
+    }
+
+    loginHistoryBlockBtns.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const url = loginHistoryBlockBtns.getAttribute("href");
+
+      // Show confirm popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+      Swal.fire({
+        text: "Are you sure you want to block this user?",
+        icon: "warning",
+        buttonsStyling: false,
+        showCancelButton: true,
+        cancelButtonText: "No, cancel",
+        confirmButtonText: "Yes, disable!",
+        customClass: {
+          confirmButton: "btn btn-primary",
+          cancelButton: "btn btn-light",
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .get(url)
+            .then((response) => {
+              // Show success message. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+              Swal.fire({
+                text:
+                  response.data.message ||
+                  "The login session has been successfully blocked!",
+                icon: "success",
+                buttonsStyling: false,
+                confirmButtonText: "Ok, got it!",
+                customClass: {
+                  confirmButton: "btn btn-primary",
+                },
+                allowOutsideClick: false,
+                showCancelButton: false,
+              }).then((outcome) => {
+                if (outcome.isConfirmed) {
+                  location.reload();
+                }
+              });
+            })
+            .catch((error) => {
+              const errors = error.response.data.message
+                ? error.response.data.message
+                : error.response.data.errors;
+
+              Swal.fire({
+                html: `${
+                  errors instanceof Array
+                    ? `<ul class="text-start">${Object.values(
+                        error.response.data.errors
+                      )
+                        .map((err) => `<li>${err?.message}</li>`)
+                        .join("")}</ul>`
+                    : errors
+                }`,
+                icon: "error",
+                buttonsStyling: false,
+                confirmButtonText: "Ok, got it!",
+                customClass: {
+                  confirmButton: "btn btn-primary",
+                },
+              });
+            });
+        }
+      });
+    });
+  };
+
+  // Sign out login session
+  const handleLoginHistorySignOut = () => {
+    if (!loginHistorySignOutBtns) {
+      return;
+    }
+
+    loginHistorySignOutBtns.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const url = loginHistorySignOutBtns.getAttribute("href");
+
+      // Show confirm popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+      Swal.fire({
+        text: "Are you sure you want to sign out this user?",
+        icon: "warning",
+        buttonsStyling: false,
+        showCancelButton: true,
+        cancelButtonText: "No, cancel",
+        confirmButtonText: "Yes, sign out!",
+        customClass: {
+          confirmButton: "btn btn-primary",
+          cancelButton: "btn btn-light",
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .get(url)
+            .then((response) => {
+              // Show success message. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+              Swal.fire({
+                text:
+                  response.data.message ||
+                  "The login session has been successfully signed out!",
+                icon: "success",
+                buttonsStyling: false,
+                confirmButtonText: "Ok, got it!",
+                customClass: {
+                  confirmButton: "btn btn-primary",
+                },
+                allowOutsideClick: false,
+                showCancelButton: false,
+              }).then((outcome) => {
+                if (outcome.isConfirmed) {
+                  location.reload();
+                }
+              });
+            })
+            .catch((error) => {
+              const errors = error.response.data.message
+                ? error.response.data.message
+                : error.response.data.errors;
+
+              Swal.fire({
+                html: `${
+                  errors instanceof Array
+                    ? `<ul class="text-start">${Object.values(
+                        error.response.data.errors
+                      )
+                        .map((err) => `<li>${err?.message}</li>`)
+                        .join("")}</ul>`
+                    : errors
+                }`,
+                icon: "error",
+                buttonsStyling: false,
+                confirmButtonText: "Ok, got it!",
+                customClass: {
+                  confirmButton: "btn btn-primary",
+                },
+              });
+            });
+        }
+      });
+    });
+  };
+
+  return {
+    // Public functions
+    init() {
+      handleLoginHistoryStatus();
+      handleLoginHistorySignOut();
+    },
+  };
+})();
+
+// On document ready
+KTUtil.onDOMContentLoaded(() => {
+  KTUsersUpdateLoginHistory.init();
+});
