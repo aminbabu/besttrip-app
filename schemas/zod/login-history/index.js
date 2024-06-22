0cec6fa648dee384a46e5787e56f7f40b5795834
Amin Babu <amin.babu.bd@gmail.com>
2024-06-22 19:44:34 +0600

    Add login history schema

---
+++ schemas/zod/login-history/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file /schemas/zod/login-history/index.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 22 June, 2024
+ * @update_date 22 June, 2024
+ */
+
+// export login history schema
+module.exports = {
+  loginHistorySchema: require("./login-history"),
+};
