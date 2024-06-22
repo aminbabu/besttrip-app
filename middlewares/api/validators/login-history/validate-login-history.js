37513a4f6ad8f9e15c67a1aebca723c6e365f928
Amin Babu <amin.babu.bd@gmail.com>
2024-06-22 19:40:16 +0600

    Add login history validators for ID and general validation

---
+++ middlewares/api/validators/login-history/validate-login-history.js
@@ -0,0 +1,29 @@
+/**
+ * @file /middlewares/api/validators/login-history/validate-login-history.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 22 June, 2024
+ * @update_date 22 June, 2024
+ */
+
+// dependencies
+const { loginHistorySchema } = require("../../../../schemas/zod/login-history");
+const { zodErrorHandler } = require("../../../../handlers/errors");
+
+// export login history validator
+module.exports = (req, res, next) => {
+  // validate request body
+  const { error, success } = loginHistoryScheme
+    .omit({ id: true })
+    .safeParse(req.params);
+
+  // check for errors
+  if (!success) {
+    // return error response
+    return zodErrorHandler(res, error);
+  }
+
+  // proceed to next middleware
+  return next();
+};
