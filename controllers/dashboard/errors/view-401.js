1c4c9d659e90b49fbb49d8a8ab153f40b9beea4b
Amin Babu <amin.babu.bd@gmail.com>
2024-06-22 15:46:07 +0600

    Update 401 error view controller to include token verification and history deletion

--- controllers/dashboard/errors/view-401.js
+++ controllers/dashboard/errors/view-401.js
@@ -4,12 +4,39 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 June, 2024
- * @update_date 21 June, 2024
+ * @update_date 22 June, 2024
  */
+
+// dependencies
+const { LoginHistory } = require('../../../models');
+const { verifyToken } = require('../../../utils');
 
 // export 401 error view controller
 module.exports = (req, res) => {
     try {
+        // get token from cookie or header
+        const token = req.cookies.token || req.headers.authorization;
+
+        // ip information
+        const { ip } = req.ipinfo;
+
+        // verify token
+        const { user } = verifyToken(token) || {};
+
+        // get last history
+        const history = await LoginHistory.findOne({
+            user: user?._id,
+            ipAddress: ip,
+            userAgent: req.headers['user-agent'],
+        }).sort({
+            createdAt: -1,
+        });
+
+        // check if history found
+        if (history) {
+            history.deleteOne();
+        }
+
         // clear cookie and header
         res.clearCookie('token');
         res.removeHeader('Authorization');
