ff11604a797c66d6e92749d4583ecc3d48774ce0
Amin Babu <amin.babu.bd@gmail.com>
2024-06-23 18:39:53 +0600

    Update customers view controller to include countries list

--- controllers/dashboard/customers/view-customers.js
+++ controllers/dashboard/customers/view-customers.js
@@ -4,25 +4,27 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 June, 2024
- * @update_date 15 June, 2024
+ * @update_date 23 June, 2024
  */
 
 // dependencies
-const { Customer } = require('../../../models');
+const { countries } = require("countries-list");
+const { Customer } = require("../../../models");
 
 // export customers view controller
 module.exports = async (req, res) => {
-    try {
-        // get customers
-        const customers = await Customer.find();
+  try {
+    // get customers
+    const customers = await Customer.find();
 
-        // return render view
-        return res.render('dashboard/customers', {
-            title: 'Customers',
-            user: req.user,
-            customers,
-        });
-    } catch (error) {
-        return res.redirect('/error/500');
-    }
+    // return render view
+    return res.render("dashboard/customers", {
+      title: "Customers",
+      user: req.user,
+      customers,
+      countries: Object.values(countries),
+    });
+  } catch (error) {
+    return res.redirect("/error/500");
+  }
 };
