e160594d42129e84f04fceb1c5dd839cf43c6c9c
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 08:17:20 +0600

    Refactor register.js controller

--- controllers/api/auth/users/register.js
+++ controllers/api/auth/users/register.js
@@ -4,67 +4,78 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 21 June, 2024
+ * @update_date 24 June, 2024
  */
 
 // dependencies
-const { welcome } = require('../../../../mails');
-const { User, Token } = require('../../../../models');
-const { generateToken, sendEmail } = require('../../../../utils');
+const { welcome } = require("../../../../mails");
+const { User, Token } = require("../../../../models");
+const { generateToken, sendEmail } = require("../../../../utils");
 
 // export register a new user controller
 module.exports = async (req, res, next) => {
-    try {
-        // get validated data
-        const { name, email, phone, password } = req.body;
+  try {
+    // get validated data
+    const { name, email, phone, password } = req.body;
 
-        // check if user already exists
-        const user = await User.findOne({ role: 'admin' });
+    // check if user already exists
+    const user = await User.findOne({ role: "admin" });
 
-        // check if user already exists
-        if (user) {
-            return res.status(400).json({
-                message: 'User with role admin already exists. Please contact support.',
-            });
-        }
-
-        // create new user
-        const newUser = new User({
-            name,
-            email,
-            phone,
-            password,
-        });
-
-        // delete existing expired tokens
-        await Token.deleteMany({
-            user: newUser._id,
-            type: 'verify-email',
-            expiresAt: { $lt: new Date() },
-        });
-
-        // generate token
-        const token = generateToken(newUser.toObject());
-        // store token in db
-        const tokenDoc = new Token({
-            user: newUser._id,
-            token,
-            type: 'verify-email',
-        });
-        await tokenDoc.save();
-
-        // send mail
-        const info = welcome({ user: newUser.toObject(), token });
-        await sendEmail(info.to, info.subject, info.text, info.html, info.attachments);
-
-        // save user
-        await newUser.save();
-
-        // return response
-        return res.status(201).json({
-            message: 'User created successfully',
-        });
-    } catch (error) {
-        return next(error);
+    // check if user already exists
+    if (user) {
+      return res.status(400).json({
+        message: "User with role admin already exists. Please contact support.",
+      });
     }
+
+    // create new user
+    const newUser = new User({
+      name,
+      email,
+      phone,
+      password,
+    });
+
+    // delete existing expired tokens
+    await Token.deleteMany({
+      user: newUser._id,
+      type: "verify-email",
+      expiresAt: { $lt: new Date() },
+    });
+
+    // generate token
+    const token = generateToken(newUser.toObject());
+
+    // store token in db
+    const tokenDoc = new Token({
+      user: newUser._id,
+      token,
+      type: "verify-email",
+    });
+
+    // prepare email
+    const info = welcome({ user: newUser.toObject(), token });
+
+    // save user
+    await newUser.save();
+
+    // save token
+    await tokenDoc.save();
+
+    // send email
+    await sendEmail(
+      info.to,
+      info.subject,
+      info.text,
+      info.html,
+      info.attachments
+    );
+
+    // return response
+    return res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
