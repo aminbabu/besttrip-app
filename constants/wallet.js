f2e5012f95c886ab4e0d1ca36911f3f6bc14f12b
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 08:40:25 +0600

    Add wallet constants

---
+++ constants/wallet.js
@@ -0,0 +1,16 @@
+/**
+ * @file /constants/wallet.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 24 June, 2024
+ * @update_date 24 June, 2024
+ */
+
+// wallet transaction types
+const WALLET_TRANSACTION_TYPES = ["top-up", "deduct"];
+
+// export constants
+module.exports = {
+  WALLET_TRANSACTION_TYPES,
+};
