a6a3a191d42bcd3d0dee4b1029350d2093b4687a
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 08:42:49 +0600

    Add wallet schema

---
+++ schemas/mongoose/wallet.js
@@ -0,0 +1,46 @@
+/**
+ * @file /schemas/mongoose/wallet.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 24 June, 2024
+ * @update_date 24 June, 2024
+ */
+
+// dependencies
+const { Schema } = require("mongoose");
+const { WALLET_TRANSACTION_TYPES } = require("../../constants");
+
+// export customer schema
+module.exports = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [() => this.user, "User is required"],
+    },
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+      required: [() => this.customer, "Customer is required"],
+    },
+    wallet: {
+      balance: {
+        type: Number,
+        required: [true, "Wallet balance is required"],
+        default: 0,
+      },
+      type: {
+        type: String,
+        enum: WALLET_TRANSACTION_TYPES,
+        default: WALLET_TRANSACTION_TYPES[0],
+      },
+      description: {
+        type: String,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
