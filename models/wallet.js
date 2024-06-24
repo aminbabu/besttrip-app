27fb713c8564720f70e63676b02c0ddc9e518c42
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 09:20:38 +0600

    Add wallet management before saving

--- models/wallet.js
+++ models/wallet.js
@@ -11,5 +11,24 @@
 const { model } = require("mongoose");
 const { walletSchema } = require("../schemas/mongoose");
 
+// manage wallet before saving
+walletSchema.pre("save", async function (next) {
+  try {
+    // check if customer is new
+    if (!this.isNew) {
+      return next();
+    }
+
+    // check if wallet is not provided
+    if (!this.balance) {
+      this.balance = 0;
+    }
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
 // export model
 module.exports = model("Wallet", walletSchema);
