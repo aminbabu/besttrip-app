c8b8aac6bc7e5c3cec8b28754ed9e1bf66b3eac2
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 07:53:08 +0600

    Add customer creation functionality and email verification

--- controllers/api/customers/create-customer.js
+++ controllers/api/customers/create-customer.js
@@ -4,12 +4,14 @@
  * @version 0.0.0
  * @author best-trip
  * @date 23 June, 2024
- * @update_date 23 June, 2024
+ * @update_date 24 June, 2024
  */
 
 // dependencies
 const { CUSTOMER_DEFAULT_PASSWORD } = require("../../../constants");
-const { Customer } = require("../../../models");
+const { welcome } = require("../../../mails");
+const { Customer, Token } = require("../../../models");
+const { sendEmail } = require("../../../utils");
 
 // export create customer controller
 module.exports = async (req, res, next) => {
@@ -17,11 +19,54 @@ module.exports = async (req, res, next) => {
     // get validated data
     const validatedData = req.body;
 
+    // get customer by email
+    const customer = await Customer.findOne({ email });
+
+    // check if customer already exists
+    if (customer) {
+      return res.status(400).json({
+        message: "Customer already exists",
+      });
+    }
+
     // create customer
-    const customer = new Customer({
+    const newCustomer = new Customer({
       ...validatedData,
       password: CUSTOMER_DEFAULT_PASSWORD,
+      wallet: {
+        balance: 0,
+      },
     });
+
+    // delete existing expired tokens
+    await Token.deleteMany({
+      customer: newCustomer._id,
+      type: "verify-email",
+      expiresAt: { $lt: new Date() },
+    });
+
+    // generate token
+    const token = generateToken(newCustomer.toObject());
+
+    // store token in db
+    const tokenDoc = new Token({
+      customer: newCustomer._id,
+      token,
+      type: "verify-email",
+    });
+
+    // save token
+    await tokenDoc.save();
+
+    // send mail
+    const info = welcome({ user: newCustomer.toObject(), token });
+    await sendEmail(
+      info.to,
+      info.subject,
+      info.text,
+      info.html,
+      info.attachments
+    );
 
     // save customer
     await customer.save();
