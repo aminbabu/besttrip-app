3efac425cf5607738aa1a35be40e7abf18ae9b34
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 08:41:16 +0600

    Remove unused constant CUSTOMER_WALLET_TRANSACTION_TYPES

--- constants/customers.js
+++ constants/customers.js
@@ -4,14 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 April, 2024
- * @update_date 23 June, 2024
+ * @update_date 24 June, 2024
  */
 
 // customer roles
 const CUSTOMER_ROLES = ["customer"];
-
-// customer wallet transaction types
-const CUSTOMER_WALLET_TRANSACTION_TYPES = ["top-up", "deduct"];
 
 // customer status
 const CUSTOMER_STATUS = ["active", "disabled"];
@@ -21,7 +18,6 @@ const CUSTOMER_DEFAULT_PASSWORD = "12345678";
 
 // export constants
 module.exports = {
-  CUSTOMER_WALLET_TRANSACTION_TYPES,
   CUSTOMER_STATUS,
   CUSTOMER_ROLES,
   CUSTOMER_DEFAULT_PASSWORD,
