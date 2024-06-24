2c4e5056eb30acc81e8236926fa8d63570653278
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 09:58:48 +0600

    Update customer wallet validation

--- middlewares/api/validators/customers/validate-customer-wallet.js
+++ middlewares/api/validators/customers/validate-customer-wallet.js
@@ -4,24 +4,27 @@
  * @version 0.0.0
  * @author best-trip
  * @date 07 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 24 June, 2024
  */
 
 // dependencies
-const { customerSchema } = require('../../../../schemas/zod/customers');
-const { zodErrorHandler } = require('../../../../handlers/errors');
+const { walletSchema } = require("../../../../schemas/zod/wallets");
+const { zodErrorHandler } = require("../../../../handlers/errors");
 
 // export validate customer wallet middleware
 module.exports = (req, res, next) => {
-    // validate request body
-    const { error, success } = customerSchema.pick({ wallet: true }).required().safeParse(req.body);
+  // validate request body
+  const { error, success } = walletSchema
+    .omit({ id: true, user: true, customer: true })
+    .required()
+    .safeParse(req.body);
 
-    // check for errors
-    if (!success) {
-        // return error response
-        return zodErrorHandler(res, error);
-    }
+  // check for errors
+  if (!success) {
+    // return error response
+    return zodErrorHandler(res, error);
+  }
 
-    // proceed to next middleware
-    return next();
+  // proceed to next middleware
+  return next();
 };
