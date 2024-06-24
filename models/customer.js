b9842cc504bb0646398a0b05bfe0964d75c2ce28
Amin Babu <amin.babu.bd@gmail.com>
2024-06-24 08:05:43 +0600

    Refactor customer model and add pre-save hooks

--- models/customer.js
+++ models/customer.js
@@ -4,57 +4,84 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 14 April, 2024
+ * @update_date 23 June, 2024
  */
 
 // dependencies
-const { model } = require('mongoose');
-const bcrypt = require('bcrypt');
-const moment = require('moment');
-const { customerSchema } = require('../schemas/mongoose');
+const { model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const moment = require("moment");
+const { customerSchema } = require("../schemas/mongoose");
 
 // hash password before saving
-customerSchema.pre('save', async function (next) {
-    try {
-        // check if password is not modified
-        if (!this.isModified('password')) {
-            return next();
-        }
-
-        // hash password
-        this.password = await bcrypt.hash(this.password, 10);
-
-        return next();
-    } catch (error) {
-        return next(error);
+customerSchema.pre("save", async function (next) {
+  try {
+    // check if password is not modified
+    if (!this.isModified("password")) {
+      return next();
     }
+
+    // hash password
+    this.password = await bcrypt.hash(this.password, 10);
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 // generate customer id before saving
-customerSchema.pre('save', async function (next) {
-    try {
-        // check if customer is new
-        if (!this.isNew) {
-            return next();
-        }
-
-        // Get the last customer ID if any
-        const lastCustomer = await this.constructor.findOne({}, {}, { sort: { createdAt: -1 } });
-
-        // get count from the last customer ID if any or set to 0
-        let count = lastCustomer ? parseInt(lastCustomer.customerID.slice(11), 10) : 0;
-
-        // check pad count with 0 if less than 4 digits and increment by 1
-        count = count > 9999 ? count + 1 : (count + 1).toString().padStart(4, '0');
-
-        // customer ID based on date (YYYYMMDD) and count (0001, 0002, ...) with prefix 'BTC'
-        this.customerID = `BTC${moment().format('YYYYMMDD')}${count}`;
-
-        return next();
-    } catch (error) {
-        return next(error);
+customerSchema.pre("save", async function (next) {
+  try {
+    // check if customer is new
+    if (!this.isNew) {
+      return next();
     }
+
+    // Get the last customer ID if any
+    const lastCustomer = await this.constructor.findOne(
+      {},
+      {},
+      { sort: { createdAt: -1 } }
+    );
+
+    // get count from the last customer ID if any or set to 0
+    let count = lastCustomer
+      ? parseInt(lastCustomer.customerID.slice(11), 10)
+      : 0;
+
+    // check pad count with 0 if less than 4 digits and increment by 1
+    count = count > 9999 ? count + 1 : (count + 1).toString().padStart(4, "0");
+
+    // customer ID based on date (YYYYMMDD) and count (0001, 0002, ...) with prefix 'BTC'
+    this.customerID = `BTC${moment().format("YYYYMMDD")}${count}`;
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// manage customer wallet before saving
+customerSchema.pre("save", async function (next) {
+  try {
+    // check if customer is new
+    if (!this.isNew) {
+      return next();
+    }
+
+    // check if wallet is not provided
+    if (!this.wallet) {
+      this.wallet = {
+        balance: 0,
+      };
+    }
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 // export model
-module.exports = model('Customer', customerSchema);
+module.exports = model("Customer", customerSchema);
