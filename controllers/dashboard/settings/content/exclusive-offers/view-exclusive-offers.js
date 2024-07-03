13c6a4ce5d922e40bbe898a01f7bce528d0023e4
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 13:55:45 +0600

    Refactor exclusiveOffers in view-exclusive-offers.js to include updatedAt timestamp

--- controllers/dashboard/settings/content/exclusive-offers/view-exclusive-offers.js
+++ controllers/dashboard/settings/content/exclusive-offers/view-exclusive-offers.js
@@ -4,17 +4,28 @@
  * @version 0.0.0
  * @author best-trip
  * @date 15 June, 2024
- * @update_date 15 June, 2024
+ * @update_date 03 Jul, 2024
  */
 
 // dependencies
 const { ExclusiveOffer } = require('../../../../../models');
+const moment = require('moment');
 
 // export exclusive offers view controller
 module.exports = async (req, res) => {
     try {
         // get exclusive offers
-        const exclusiveOffers = await ExclusiveOffer.find();
+        let exclusiveOffers = await ExclusiveOffer.find();
+
+        exclusiveOffers = exclusiveOffers.map((offer) => {
+            const modifiedOffer = { ...offer.toObject() };
+
+            modifiedOffer.updatedAt = moment(offer.updatedAt).format(
+                'DD MMM, YYYY hh:mm a'
+            );
+
+            return modifiedOffer;
+        });
 
         // render exclusive offers view
         return res.render('dashboard/settings/content/exclusive-offers', {
