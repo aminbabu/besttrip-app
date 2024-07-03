122b9b7f04fb1e6005dc89d84b153fc993657d9f
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 11:31:55 +0600

    Update meta setting date

--- controllers/api/settings/site/meta/update-meta-setting.js
+++ controllers/api/settings/site/meta/update-meta-setting.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 17 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 03 Jul, 2024
  */
 
 // dependencies
@@ -18,12 +18,12 @@ module.exports = async (req, res, next) => {
         const { name, content } = req.body;
 
         // find the existing meta settings
-        const metaSetting = await MetaSettings.findOne({ _id: id, name });
+        const metaSetting = await MetaSettings.findOne({ _id: id });
 
         // check if meta settings exists
         if (!metaSetting) {
             return res.status(404).json({
-                message: `Meta settings with name - ${name} does not exist`,
+                message: 'Meta settings not found',
             });
         }
 
