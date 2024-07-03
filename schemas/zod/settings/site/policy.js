94b2b2cce8cc6407b02873f71c8903350e56c03e
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 07:44:59 +0600

    Update site policy schema

--- schemas/zod/settings/site/policy.js
+++ schemas/zod/settings/site/policy.js
@@ -4,32 +4,60 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 April, 2024
- * @update_date 16 April, 2024
+ * @update_date 03 Jul, 2024
  */
 
 // dependencies
 const { z } = require('zod');
-const { POLICY_KEYS } = require('../../../../constants');
 
 // export site policy settings schema
 module.exports = z
     .object({
-        key: z
+        'about-us': z
             .string({
-                required_error: 'Policy key is required',
-                invalid_type_error: 'Please provide a valid policy key',
-            })
-            .refine((key) => POLICY_KEYS.includes(key), {
-                message: 'Please provide a valid policy key',
-            }),
-        content: z
-            .string({
-                required_error: 'Content is required',
-                invalid_type_error: 'Please provide a valid content',
+                required_error: 'About us content is required',
+                invalid_type_error: 'Please provide valid about us content',
             })
             .trim()
             .min(3, {
-                message: 'Content should have at least 3 characters',
-            }),
+                message: 'About us content should have at least 3 characters',
+            })
+            .optional(),
+        'terms-of-conditions': z
+            .string({
+                required_error: 'Terms of conditions content is required',
+                invalid_type_error:
+                    'Please provide valid terms of conditions content',
+            })
+            .trim()
+            .min(3, {
+                message:
+                    'Terms of conditions content should have at least 3 characters',
+            })
+            .optional(),
+        'refund-policy': z
+            .string({
+                required_error: 'Refund policy content is required',
+                invalid_type_error:
+                    'Please provide valid refund policy content',
+            })
+            .trim()
+            .min(3, {
+                message:
+                    'Refund policy content should have at least 3 characters',
+            })
+            .optional(),
+        'privacy-policy': z
+            .string({
+                required_error: 'Privacy policy content is required',
+                invalid_type_error:
+                    'Please provide valid privacy policy content',
+            })
+            .trim()
+            .min(3, {
+                message:
+                    'Privacy policy content should have at least 3 characters',
+            })
+            .optional(),
     })
     .strict();
