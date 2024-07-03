b963942db74abf6b04069746863b9fcce45db9ed
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 09:01:27 +0600

    Update policy form and CKEditor configuration

--- public/src/js/custom/apps/settings/site/policy.js
+++ public/src/js/custom/apps/settings/site/policy.js
@@ -3,7 +3,8 @@
 // Class definition
 var KTSettingsPolicy = (function () {
     // Shared variables
-    const form = document.querySelector('#kt_settings_contact_form');
+    const form = document.querySelector('#kt_settings_policy_form');
+    const editors = {};
 
     if (!form) {
         return;
@@ -13,29 +14,7 @@ var KTSettingsPolicy = (function () {
     var initSettingsPaymentAdd = () => {
         // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
         var validator = FormValidation.formValidation(form, {
-            fields: {
-                email: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Email is required',
-                        },
-                    },
-                },
-                phone: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Phone is required',
-                        },
-                    },
-                },
-                address: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Address is required',
-                        },
-                    },
-                },
-            },
+            fields: {},
 
             plugins: {
                 trigger: new FormValidation.plugins.Trigger(),
@@ -49,7 +28,7 @@ var KTSettingsPolicy = (function () {
 
         // Submit button handler
         const submitButton = form.querySelector(
-            '[kt-settings-contact-form-action="submit"]'
+            '[kt-settings-policy-form-action="submit"]'
         );
         submitButton.addEventListener('click', (e) => {
             e.preventDefault();
@@ -64,27 +43,22 @@ var KTSettingsPolicy = (function () {
                         // Disable button to avoid multiple click
                         submitButton.disabled = true;
 
-                        // Get form data
-                        const formData = new FormData(form);
-
-                        // Prepare data by filtering out empty fields
-                        const data = Array.from(formData.entries()).reduce(
-                            (acc, item) => {
-                                if (item[1]) {
-                                    acc[item[0]] = item[1];
-                                }
-                                return acc;
-                            },
-                            {}
-                        );
-
                         // Check axios library docs: https://axios-http.com/docs/intro
                         axios
                             .post(
                                 submitButton
                                     .closest('form')
                                     .getAttribute('action'),
-                                data
+                                {
+                                    'about-us':
+                                        editors.kt_docs_ckeditor_about_us.getData(),
+                                    'terms-and-conditions':
+                                        editors.kt_docs_ckeditor_toc.getData(),
+                                    'refund-policy':
+                                        editors.kt_docs_ckeditor_refund_policy.getData(),
+                                    'privacy-policy':
+                                        editors.kt_docs_ckeditor_privacy_policy.getData(),
+                                }
                             )
                             .then((response) => {
                                 if (response) {
@@ -94,8 +68,8 @@ var KTSettingsPolicy = (function () {
                                     // Show error popup. For more info check the plugin's official documentation: https://sweetalert2.github.io/
                                     Swal.fire({
                                         text:
-                                            response.data.message ||
-                                            'General settings updated successfully.',
+                                            response?.data?.message ||
+                                            'Policy settings updated successfully.',
                                         icon: 'success',
                                         buttonsStyling: false,
                                         confirmButtonText: 'Ok, got it!',
@@ -176,6 +150,85 @@ var KTSettingsPolicy = (function () {
         });
     };
 
+    class CKEditorUploadAdapter {
+        constructor(loader) {
+            this.loader = loader;
+        }
+
+        // Starts the upload process.
+        upload() {
+            return this.loader.file.then(
+                (file) =>
+                    new Promise((resolve, reject) => {
+                        this._initRequest();
+                        this._initListeners(resolve, reject, file);
+                        this._sendRequest(file);
+                    })
+            );
+        }
+
+        // Aborts the upload process.
+        abort() {
+            if (this.xhr) {
+                this.xhr.abort();
+            }
+        }
+
+        // Initializes XMLHttpRequest.
+        _initRequest() {
+            this.xhr = new XMLHttpRequest();
+            this.xhr.open('POST', '/api/settings/site/policy/media', true);
+            this.xhr.responseType = 'json';
+        }
+
+        // Initializes listeners for XMLHttpRequest.
+        _initListeners(resolve, reject, file) {
+            const xhr = this.xhr;
+            const loader = this.loader;
+            const genericErrorText = `Couldn't upload file: ${file.name}.`;
+
+            xhr.addEventListener('error', () => reject(genericErrorText));
+            xhr.addEventListener('abort', () => reject());
+            xhr.addEventListener('load', () => {
+                const response = xhr.response;
+
+                if (!response || response.error) {
+                    return reject(
+                        response && response.error
+                            ? response.error.message
+                            : genericErrorText
+                    );
+                }
+
+                resolve({
+                    default: response.url,
+                });
+            });
+
+            if (xhr.upload) {
+                xhr.upload.addEventListener('progress', (evt) => {
+                    if (evt.lengthComputable) {
+                        loader.uploadTotal = evt.total;
+                        loader.uploaded = evt.loaded;
+                    }
+                });
+            }
+        }
+
+        // Sends the request.
+        _sendRequest(file) {
+            const data = new FormData();
+            data.append('upload', file);
+            this.xhr.send(data);
+        }
+    }
+
+    function CKEditorUploadAdapterPlugin(editor) {
+        editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+            return new CKEditorUploadAdapter(loader);
+        };
+    }
+
     // CKEditors
     const initCKEditors = () => {
         const textareas = [
@@ -186,9 +239,11 @@ var KTSettingsPolicy = (function () {
         ];
 
         textareas.forEach((id) => {
-            ClassicEditor.create(document.getElementById(id))
+            ClassicEditor.create(document.getElementById(id), {
+                extraPlugins: [CKEditorUploadAdapterPlugin],
+            })
                 .then((editor) => {
-                    // console.log(editor);
+                    editors[id] = editor;
                 })
                 .catch((error) => {
                     console.error(error);
