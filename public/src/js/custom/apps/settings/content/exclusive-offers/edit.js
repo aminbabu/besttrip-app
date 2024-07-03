7265600c9d27d15a1ca9ab5e6f62c6176c9bbce2
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 14:14:42 +0600

    Remove file uploader initialization from init function

--- public/src/js/custom/apps/settings/content/exclusive-offers/edit.js
+++ public/src/js/custom/apps/settings/content/exclusive-offers/edit.js
@@ -1,257 +1,195 @@
-"use strict";
+'use strict';
 
 // Class definition
 var KTContentExclusiveOfferEdit = (function () {
-  // Shared variables
-  const element = document.getElementById(
-    "kt_content_exclusive_offers_edit_modal"
-  );
-  const form = element.querySelector("#kt_content_exclusive_offers_edit_form");
-  const modal = new bootstrap.Modal(element);
-  var ktFileUploaderContent;
-
-  // Init edit modal
-  var initEditContentExclusiveOffer = () => {
-    // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
-    var validator = FormValidation.formValidation(form, {
-      fields: {
-        thumbnail: {
-          validators: {
-            notEmpty: {
-              message: "Thumbnail is required",
-            },
-          },
-        },
-        link: {
-          validators: {
-            notEmpty: {
-              message: "Link is required",
-            },
-          },
-        },
-        status: {
-          validators: {
-            notEmpty: {
-              message: "Status is required",
-            },
-          },
-        },
-      },
-
-      plugins: {
-        trigger: new FormValidation.plugins.Trigger(),
-        bootstrap: new FormValidation.plugins.Bootstrap5({
-          rowSelector: ".fv-row",
-          eleInvalidClass: "",
-          eleValidClass: "",
-        }),
-      },
-    });
-
-    // Submit button handler
-    const submitButton = element.querySelector(
-      "#kt_content_exclusive_offers_edit_submit"
+    // Shared variables
+    const element = document.getElementById(
+        'kt_content_exclusive_offers_edit_modal'
     );
-    submitButton.addEventListener("click", (e) => {
-      e.preventDefault();
+    const form = element.querySelector(
+        '#kt_content_exclusive_offers_edit_form'
+    );
+    const modal = new bootstrap.Modal(element);
+    var ktFileUploaderContent;
 
-      // Validate form before submit
-      if (validator) {
-        validator.validate().then(function (status) {
-          if (status == "Valid") {
-            // Show loading indication
-            submitButton.setAttribute("data-kt-indicator", "on");
-
-            // Disable button to avoid multiple click
-            submitButton.disabled = true;
-
-            // Simulate form submission. For more info check the plugin's official documentation: https://sweetalert2.github.io/
-            setTimeout(function () {
-              // Remove loading indication
-              submitButton.removeAttribute("data-kt-indicator");
-
-              // Enable button
-              submitButton.disabled = false;
-
-              // Show popup confirmation
-              Swal.fire({
-                text: "Form has been successfully submitted!",
-                icon: "success",
-                buttonsStyling: false,
-                confirmButtonText: "Ok, got it!",
-                customClass: {
-                  confirmButton: "btn btn-primary",
+    // Init edit modal
+    var initEditContentExclusiveOffer = () => {
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+        var validator = FormValidation.formValidation(form, {
+            fields: {
+                thumbnail: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Thumbnail is required',
+                        },
+                    },
                 },
-              }).then(function (result) {
-                if (result.isConfirmed) {
-                  modal.hide();
-                }
-              });
+                link: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Link is required',
+                        },
+                    },
+                },
+                status: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Status is required',
+                        },
+                    },
+                },
+            },
 
-              //form.submit(); // Submit form
-            }, 2000);
-          } else {
-            // Show popup warning. For more info check the plugin's official documentation: https://sweetalert2.github.io/
-            Swal.fire({
-              text: "Sorry, looks like there are some errors detected, please try again.",
-              icon: "error",
-              buttonsStyling: false,
-              confirmButtonText: "Ok, got it!",
-              customClass: {
-                confirmButton: "btn btn-primary",
-              },
-            });
-          }
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap5({
+                    rowSelector: '.fv-row',
+                    eleInvalidClass: '',
+                    eleValidClass: '',
+                }),
+            },
         });
-      }
-    });
 
-    // Cancel button handler
-    const cancelButton = element.querySelector(
-      "#kt_content_exclusive_offers_edit_cancel"
-    );
-    cancelButton.addEventListener("click", (e) => {
-      e.preventDefault();
+        // Submit button handler
+        const submitButton = element.querySelector(
+            '#kt_content_exclusive_offers_edit_submit'
+        );
+        submitButton.addEventListener('click', (e) => {
+            e.preventDefault();
 
-      Swal.fire({
-        text: "Are you sure you would like to cancel?",
-        icon: "warning",
-        showCancelButton: true,
-        buttonsStyling: false,
-        confirmButtonText: "Yes, cancel it!",
-        cancelButtonText: "No, return",
-        customClass: {
-          confirmButton: "btn btn-primary",
-          cancelButton: "btn btn-active-light",
-        },
-      }).then(function (result) {
-        if (result.value) {
-          form.reset(); // Reset form
-          modal.hide();
-        } else if (result.dismiss === "cancel") {
-          Swal.fire({
-            text: "Your form has not been cancelled!.",
-            icon: "error",
-            buttonsStyling: false,
-            confirmButtonText: "Ok, got it!",
-            customClass: {
-              confirmButton: "btn btn-primary",
-            },
-          });
-        }
-      });
-    });
+            // Validate form before submit
+            if (validator) {
+                validator.validate().then(function (status) {
+                    if (status == 'Valid') {
+                        // Show loading indication
+                        submitButton.setAttribute('data-kt-indicator', 'on');
 
-    // Close button handler
-    const closeButton = element.querySelector(
-      "#kt_content_exclusive_offers_edit_close"
-    );
-    closeButton.addEventListener("click", (e) => {
-      e.preventDefault();
+                        // Disable button to avoid multiple click
+                        submitButton.disabled = true;
 
-      Swal.fire({
-        text: "Are you sure you would like to cancel?",
-        icon: "warning",
-        showCancelButton: true,
-        buttonsStyling: false,
-        confirmButtonText: "Yes, cancel it!",
-        cancelButtonText: "No, return",
-        customClass: {
-          confirmButton: "btn btn-primary",
-          cancelButton: "btn btn-active-light",
-        },
-      }).then(function (result) {
-        if (result.value) {
-          form.reset(); // Reset form
-          modal.hide();
-        } else if (result.dismiss === "cancel") {
-          Swal.fire({
-            text: "Your form has not been cancelled!.",
-            icon: "error",
-            buttonsStyling: false,
-            confirmButtonText: "Ok, got it!",
-            customClass: {
-              confirmButton: "btn btn-primary",
-            },
-          });
-        }
-      });
-    });
-  };
+                        // Simulate form submission. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                        setTimeout(function () {
+                            // Remove loading indication
+                            submitButton.removeAttribute('data-kt-indicator');
 
-  // Init File Uploader
-  const initFileUploader = () => {
-    const handleFileInputChange = (input) => {
-      const previewContainer = input.closest(".kt-file-uploader");
-      const label = previewContainer.querySelector(".kt-file-uploader-label");
-      const maxFileSize = parseInt(
-        input.getAttribute("data-kt-file-uploader-max-size")
-      );
-      const invalidMessage =
-        previewContainer.querySelector(".invalid-feedback");
-      const file = input.files[0];
+                            // Enable button
+                            submitButton.disabled = false;
 
-      if (!file) {
-        return;
-      }
+                            // Show popup confirmation
+                            Swal.fire({
+                                text: 'Form has been successfully submitted!',
+                                icon: 'success',
+                                buttonsStyling: false,
+                                confirmButtonText: 'Ok, got it!',
+                                customClass: {
+                                    confirmButton: 'btn btn-primary',
+                                },
+                            }).then(function (result) {
+                                if (result.isConfirmed) {
+                                    modal.hide();
+                                }
+                            });
 
-      // remove error message
-      invalidMessage.classList.add("d-none");
+                            //form.submit(); // Submit form
+                        }, 2000);
+                    } else {
+                        // Show popup warning. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                        Swal.fire({
+                            text: 'Sorry, looks like there are some errors detected, please try again.',
+                            icon: 'error',
+                            buttonsStyling: false,
+                            confirmButtonText: 'Ok, got it!',
+                            customClass: {
+                                confirmButton: 'btn btn-primary',
+                            },
+                        });
+                    }
+                });
+            }
+        });
 
-      // create preview element and append to the container
-      const preview = document.createElement("img");
+        // Cancel button handler
+        const cancelButton = element.querySelector(
+            '#kt_content_exclusive_offers_edit_cancel'
+        );
+        cancelButton.addEventListener('click', (e) => {
+            e.preventDefault();
 
-      if (file?.size > 1024 * 1024 * maxFileSize) {
-        const message = `File size should not exceed ${maxFileSize}MB`;
+            Swal.fire({
+                text: 'Are you sure you would like to cancel?',
+                icon: 'warning',
+                showCancelButton: true,
+                buttonsStyling: false,
+                confirmButtonText: 'Yes, cancel it!',
+                cancelButtonText: 'No, return',
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-active-light',
+                },
+            }).then(function (result) {
+                if (result.value) {
+                    form.reset(); // Reset form
+                    modal.hide();
+                } else if (result.dismiss === 'cancel') {
+                    Swal.fire({
+                        text: 'Your form has not been cancelled!.',
+                        icon: 'error',
+                        buttonsStyling: false,
+                        confirmButtonText: 'Ok, got it!',
+                        customClass: {
+                            confirmButton: 'btn btn-primary',
+                        },
+                    });
+                }
+            });
+        });
 
-        if (ktFileUploaderContent) {
-          // restore label content
-          label.innerHTML = ktFileUploaderContent;
-        }
+        // Close button handler
+        const closeButton = element.querySelector(
+            '#kt_content_exclusive_offers_edit_close'
+        );
+        closeButton.addEventListener('click', (e) => {
+            e.preventDefault();
 
-        // empty input value
-        input.value = "";
-
-        // append error message
-        invalidMessage.innerHTML = message;
-        invalidMessage.classList.remove("d-none");
-        return;
-      }
-
-      // add was-invalid class to the container
-      if (!previewContainer.classList.contains("was-invalided")) {
-        ktFileUploaderContent = label.innerHTML;
-        previewContainer.classList.add("was-invalided");
-      }
-
-      // remove content and append preview
-      label.innerHTML = "";
-      preview.classList.add("kt-file-uploader-preview");
-      preview.src = URL.createObjectURL(file);
-      preview.alt = file.name;
-      label.appendChild(preview);
+            Swal.fire({
+                text: 'Are you sure you would like to cancel?',
+                icon: 'warning',
+                showCancelButton: true,
+                buttonsStyling: false,
+                confirmButtonText: 'Yes, cancel it!',
+                cancelButtonText: 'No, return',
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-active-light',
+                },
+            }).then(function (result) {
+                if (result.value) {
+                    form.reset(); // Reset form
+                    modal.hide();
+                } else if (result.dismiss === 'cancel') {
+                    Swal.fire({
+                        text: 'Your form has not been cancelled!.',
+                        icon: 'error',
+                        buttonsStyling: false,
+                        confirmButtonText: 'Ok, got it!',
+                        customClass: {
+                            confirmButton: 'btn btn-primary',
+                        },
+                    });
+                }
+            });
+        });
     };
 
-    form.addEventListener("change", function (event) {
-      const target = event.target;
-
-      // Check if the changed element is an input with type file
-      if (target.tagName === "INPUT" && target.type === "file") {
-        handleFileInputChange(target);
-      }
-    });
-  };
-
-  return {
-    // Public functions
-    init: function () {
-      initEditContentExclusiveOffer();
-      initFileUploader();
-    },
-  };
+    return {
+        // Public functions
+        init: function () {
+            initEditContentExclusiveOffer();
+        },
+    };
 })();
 
 // On document ready
 KTUtil.onDOMContentLoaded(function () {
-  KTContentExclusiveOfferEdit.init();
+    KTContentExclusiveOfferEdit.init();
 });
