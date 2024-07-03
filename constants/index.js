607f50575d978be504a0a24769867bba755e16e1
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 07:44:38 +0600

    Refactor constants/index.js and remove constants/policies.js

--- constants/index.js
+++ constants/index.js
@@ -4,31 +4,30 @@
  * @version 0.0.0
  * @author best-trip
  * @date 07 April, 2024
- * @update_date 24 June, 2024
+ * @update_date 03 Jul, 2024
  */
 
 // export constants
 module.exports = {
-  ...require("./globals"),
-  ...require("./media"),
-  ...require("./xss-whitelist"),
-  ...require("./users"),
-  ...require("./customers"),
-  ...require("./policies"),
-  ...require("./payments"),
-  ...require("./sections"),
-  ...require("./exclusive-offers"),
-  ...require("./hotel-offers"),
-  ...require("./flight-offers"),
-  ...require("./umrah-offers"),
-  ...require("./blog-posts"),
-  ...require("./themes"),
-  ...require("./payment-requests"),
-  ...require("./umrah-pacakges"),
-  ...require("./umrah-package-durations"),
-  ...require("./umrah-package-types"),
-  ...require("./visa"),
-  ...require("./login-history"),
-  ...require("./umrah-bookings"),
-  ...require("./wallet"),
+    ...require('./globals'),
+    ...require('./media'),
+    ...require('./xss-whitelist'),
+    ...require('./users'),
+    ...require('./customers'),
+    ...require('./payments'),
+    ...require('./sections'),
+    ...require('./exclusive-offers'),
+    ...require('./hotel-offers'),
+    ...require('./flight-offers'),
+    ...require('./umrah-offers'),
+    ...require('./blog-posts'),
+    ...require('./themes'),
+    ...require('./payment-requests'),
+    ...require('./umrah-pacakges'),
+    ...require('./umrah-package-durations'),
+    ...require('./umrah-package-types'),
+    ...require('./visa'),
+    ...require('./login-history'),
+    ...require('./umrah-bookings'),
+    ...require('./wallet'),
 };
