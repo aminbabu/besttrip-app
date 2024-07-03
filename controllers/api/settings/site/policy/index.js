7b4e09ec1bce567ea12c5c9647ffa6d9539ee94d
Amin Babu <amin.babu.bd@gmail.com>
2024-07-03 07:46:23 +0600

    Update site policy settings controllers

--- controllers/api/settings/site/policy/index.js
+++ controllers/api/settings/site/policy/index.js
@@ -4,13 +4,11 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 April, 2024
- * @update_date 19 April, 2024
+ * @update_date 03 Jul, 2024
  */
 
 // export site policy settings controllers
 module.exports = {
     getPolicies: require('./get-policies'),
-    getPolicy: require('./get-policy'),
-    createPolicy: require('./create-policy'),
-    updatePolicy: require('./update-policy'),
+    updateOrCreagtePolicy: require('./update-or-create-policy'),
 };
