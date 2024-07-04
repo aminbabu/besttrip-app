e182cf7f090ab599a52cfffd233682a855afdf22
Amin Babu <amin.babu.bd@gmail.com>
2024-07-04 10:50:36 +0600

    Update dependencies and configuration

--- app.js
+++ app.js
@@ -5,34 +5,34 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 07 June, 2024
+ * @update_date 04 Jul, 2024
  */
 
 // dependencies
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const { default: xssInstance } = require("xss-shield");
-const cors = require("cors");
-const expressFileUpload = require("express-fileupload");
-const { expressCspHeader } = require("express-csp-header");
-const { default: ipinfo } = require("ipinfo-express");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const { default: xssInstance } = require('xss-shield');
+const cors = require('cors');
+const expressFileUpload = require('express-fileupload');
+const { expressCspHeader } = require('express-csp-header');
+const { default: ipinfo } = require('ipinfo-express');
 
 // config
 const {
-  mongoDB,
-  env,
-  expressFileUploadConf,
-  cspDirectives,
-  ipInfo,
-} = require("./config");
+    mongoDB,
+    env,
+    expressFileUploadConf,
+    cspDirectives,
+    ipInfo,
+} = require('./config');
 
 // constants
-const { WHITE_LIST } = require("./constants");
+const { WHITE_LIST } = require('./constants');
 
 // dotenv config
 dotenv.config();
@@ -44,18 +44,18 @@ const app = express();
 mongoDB.connect();
 
 // trust proxy
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // middlewares
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(env.COOKIE_SECRET));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 app.use(cors());
 app.use(expressFileUpload(expressFileUploadConf));
@@ -63,30 +63,35 @@ app.use(xssInstance.xssShield(WHITE_LIST));
 app.use(expressCspHeader(cspDirectives));
 app.use(ipinfo(ipInfo));
 
+app.use((req, res, next) => {
+    res.locals.url = req.originalUrl;
+    next();
+});
+
 // routes
-app.use("/api", require("./routes/api"));
-app.use("/dashboard", require("./routes/dashboard"));
+app.use('/api', require('./routes/api'));
+app.use('/dashboard', require('./routes/dashboard'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.log(err);
+    console.log(err);
 
-  // send error response
-  return res.status(err.status || 500).json({
-    message: err.message,
-  });
+    // send error response
+    return res.status(err.status || 500).json({
+        message: err.message,
+    });
 
-  // render the error page
-  // return res.status(err.status || 500).render('error');
+    // render the error page
+    // return res.status(err.status || 500).render('error');
 });
 
 module.exports = app;
