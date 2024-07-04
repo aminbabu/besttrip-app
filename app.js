e26905865dc59472ffc57ad555568fbb470cdad6
Amin Babu <amin.babu.bd@gmail.com>
2024-07-04 11:01:16 +0600

    Remove unused dependency

--- app.js
+++ app.js
@@ -9,7 +9,6 @@
  */
 
 // dependencies
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
