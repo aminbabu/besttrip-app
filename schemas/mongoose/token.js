b4cbe59bb42e01391bf7b6f275c4f1ede622af40
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 15:22:29 +0600

    Update token.js with new update_date

--- schemas/mongoose/token.js
+++ schemas/mongoose/token.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 April, 2024
- * @update_date 14 April, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
@@ -16,12 +16,10 @@ module.exports = new Schema(
         user: {
             type: Schema.Types.ObjectId,
             ref: 'User',
-            required: [() => this.user, 'User is required'],
         },
         customer: {
             type: Schema.Types.ObjectId,
             ref: 'Customer',
-            required: [() => this.customer, 'Customer is required'],
         },
         token: {
             type: String,
