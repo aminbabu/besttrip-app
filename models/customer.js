9a7149eb49e7d9ced2c048986a50da9f70d78f32
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 16:34:09 +0600

    Refactor customer model and add pre-save hooks

--- models/customer.js
+++ models/customer.js
@@ -4,89 +4,110 @@
  * @version 0.0.0
  * @author best-trip
  * @date 18 March, 2024
- * @update_date 24 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
-const { model } = require("mongoose");
-const bcrypt = require("bcrypt");
-const moment = require("moment");
-const { customerSchema } = require("../schemas/mongoose");
-const Wallet = require("./wallet");
+const { model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const moment = require('moment');
+const { customerSchema } = require('../schemas/mongoose');
+const Wallet = require('./wallet');
 
 // hash password before saving
-customerSchema.pre("save", async function (next) {
-  try {
-    // check if password is not modified
-    if (!this.isModified("password")) {
-      return next();
+customerSchema.pre('save', async function (next) {
+    try {
+        // check if password is not modified
+        if (!this.isModified('password')) {
+            return next();
+        }
+
+        // hash password
+        this.password = await bcrypt.hash(this.password, 10);
+
+        return next();
+    } catch (error) {
+        return next(error);
     }
-
-    // hash password
-    this.password = await bcrypt.hash(this.password, 10);
-
-    return next();
-  } catch (error) {
-    return next(error);
-  }
 });
 
 // generate customer id before saving
-customerSchema.pre("save", async function (next) {
-  try {
-    // check if customer is new
-    if (!this.isNew) {
-      return next();
+customerSchema.pre('save', async function (next) {
+    try {
+        // check if customer is new
+        if (!this.isNew) {
+            return next();
+        }
+
+        // Get the last customer ID if any
+        const lastCustomer = await this.constructor.findOne(
+            {},
+            {},
+            { sort: { createdAt: -1 } }
+        );
+
+        // get count from the last customer ID if any or set to 0
+        let count = lastCustomer
+            ? parseInt(lastCustomer.customerID.slice(11), 10)
+            : 0;
+
+        // check pad count with 0 if less than 4 digits and increment by 1
+        count =
+            count > 9999 ? count + 1 : (count + 1).toString().padStart(4, '0');
+
+        // customer ID based on date (YYYYMMDD) and count (0001, 0002, ...) with prefix 'BTC'
+        this.customerID = `BTC${moment().format('YYYYMMDD')}${count}`;
+
+        return next();
+    } catch (error) {
+        return next(error);
     }
-
-    // Get the last customer ID if any
-    const lastCustomer = await this.constructor.findOne(
-      {},
-      {},
-      { sort: { createdAt: -1 } }
-    );
-
-    // get count from the last customer ID if any or set to 0
-    let count = lastCustomer
-      ? parseInt(lastCustomer.customerID.slice(11), 10)
-      : 0;
-
-    // check pad count with 0 if less than 4 digits and increment by 1
-    count = count > 9999 ? count + 1 : (count + 1).toString().padStart(4, "0");
-
-    // customer ID based on date (YYYYMMDD) and count (0001, 0002, ...) with prefix 'BTC'
-    this.customerID = `BTC${moment().format("YYYYMMDD")}${count}`;
-
-    return next();
-  } catch (error) {
-    return next(error);
-  }
 });
 
 // manage customer wallet before saving
-customerSchema.pre("save", async function (next) {
-  try {
-    // check if customer is new
-    if (!this.isNew) {
-      return next();
+customerSchema.pre('save', async function (next) {
+    try {
+        // check if customer is new
+        if (!this.isNew) {
+            return next();
+        }
+
+        // create wallet for customer
+        const wallet = new Wallet({
+            customer: this._id,
+        });
+
+        // save wallet
+        await wallet.save();
+
+        // set wallet id
+        this.wallet = wallet._id;
+
+        return next();
+    } catch (error) {
+        return next(error);
     }
+});
 
-    // create wallet for customer
-    const wallet = new Wallet({
-      customer: this._id,
-    });
+// delete customer wallet before deleting
+customerSchema.pre('remove', async function (next) {
+    try {
+        // get customer's wallet
+        const wallet = await Wallet.findOne({ customer: this._id });
 
-    // save wallet
-    await wallet.save();
+        // check if wallet exists
+        if (!wallet) {
+            return next();
+        }
 
-    // set wallet id
-    this.wallet = wallet._id;
+        // delete wallet
+        await wallet.deleteOne();
 
-    return next();
-  } catch (error) {
-    return next(error);
-  }
+        return next();
+    } catch (error) {
+        return next(error);
+    }
 });
 
 // export model
-module.exports = model("Customer", customerSchema);
+module.exports = model('Customer', customerSchema);
