d2beaa2b1e67f586e924afafe1d0fb7e9bda5998
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 15:10:21 +0600

    Update wallet.js with new date format

--- models/wallet.js
+++ models/wallet.js
@@ -4,31 +4,26 @@
  * @version 0.0.0
  * @author best-trip
  * @date 24 June, 2024
- * @update_date 24 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
-const { model } = require("mongoose");
-const { walletSchema } = require("../schemas/mongoose");
+const { model } = require('mongoose');
+const { walletSchema } = require('../schemas/mongoose');
 
 // manage wallet before saving
-walletSchema.pre("save", async function (next) {
-  try {
-    // check if customer is new
-    if (!this.isNew) {
-      return next();
-    }
+walletSchema.pre('save', async function (next) {
+    try {
+        // check if customer is new
+        if (!this.isNew) {
+            return next();
+        }
 
-    // check if wallet is not provided
-    if (!this.balance) {
-      this.balance = 0;
+        return next();
+    } catch (error) {
+        return next(error);
     }
-
-    return next();
-  } catch (error) {
-    return next(error);
-  }
 });
 
 // export model
-module.exports = model("Wallet", walletSchema);
+module.exports = model('Wallet', walletSchema);
