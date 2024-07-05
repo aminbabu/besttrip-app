4bddad83512a7a2385881d6496a9565c9a297e0e
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 09:16:09 +0600

    Update customer schema in mongoose

--- schemas/mongoose/customer.js
+++ schemas/mongoose/customer.js
@@ -4,95 +4,107 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 April, 2024
- * @update_date 25 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
-const { Schema } = require("mongoose");
-const { CUSTOMER_ROLES, CUSTOMER_STATUS } = require("../../constants");
+const { Schema } = require('mongoose');
+const { CUSTOMER_ROLES, CUSTOMER_STATUS } = require('../../constants');
 
 // export customer schema
 module.exports = new Schema(
-  {
-    customerID: {
-      type: String,
-      unique: [true, "Customer ID already exists"],
+    {
+        customerID: {
+            type: String,
+            unique: [true, 'Customer ID already exists'],
+        },
+        avatar: {
+            type: String,
+        },
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: [true, 'Email already exists'],
+            lowercase: true,
+        },
+        phone: {
+            type: String,
+            required: [true, 'Phone is required'],
+            unique: [true, 'Phone already exists'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            select: false,
+        },
+        role: {
+            type: String,
+            enum: CUSTOMER_ROLES,
+            default: CUSTOMER_ROLES[0],
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        status: {
+            type: String,
+            enum: CUSTOMER_STATUS,
+            default: CUSTOMER_STATUS[1],
+        },
+        twoStepAuth: {
+            type: Boolean,
+            default: false,
+        },
+        dob: {
+            type: Date,
+        },
+        address: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        state: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        postalCode: {
+            type: String,
+        },
+        flyerNumber: {
+            type: String,
+        },
+        wallet: {
+            type: Schema.Types.ObjectId,
+            ref: 'Wallet',
+            unique: [true, 'Wallet already exists'],
+        },
+        bookings: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'UmrahBooking',
+            },
+        ],
+        paymentRequests: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'PaymentRequest',
+            },
+        ],
+        loginHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'LoginHistory',
+            },
+        ],
     },
-    avatar: {
-      type: String,
-    },
-    name: {
-      type: String,
-      required: [true, "Name is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: [true, "Email already exists"],
-      lowercase: true,
-    },
-    phone: {
-      type: String,
-      required: [true, "Phone is required"],
-      unique: [true, "Phone already exists"],
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-      select: false,
-    },
-    role: {
-      type: String,
-      enum: CUSTOMER_ROLES,
-      default: CUSTOMER_ROLES[0],
-    },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    status: {
-      type: String,
-      enum: CUSTOMER_STATUS,
-      default: CUSTOMER_STATUS[1],
-    },
-    twoStepAuth: {
-      type: Boolean,
-      default: false,
-    },
-    dob: {
-      type: Date,
-    },
-    address: {
-      type: String,
-    },
-    city: {
-      type: String,
-    },
-    state: {
-      type: String,
-    },
-    country: {
-      type: String,
-    },
-    postalCode: {
-      type: String,
-    },
-    flyerNumber: {
-      type: String,
-    },
-    wallet: {
-      type: Schema.Types.ObjectId,
-      ref: "Wallet",
-      unique: [true, "Wallet already exists"],
-    },
-    loginHistory: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "LoginHistory",
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
