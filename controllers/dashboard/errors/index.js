8ef88101388a56e77b9d88b8a1a95fbf8536eb0e
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 08:42:16 +0600

    Add view-403 error controller

--- controllers/dashboard/errors/index.js
+++ controllers/dashboard/errors/index.js
@@ -4,12 +4,13 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 June, 2024
- * @update_date 03 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // export error controllers
 module.exports = {
     view401: require('./view-401'),
+    view403: require('./view-403'),
     view404: require('./view-404'),
     view500: require('./view-500'),
 };
---
+++ controllers/dashboard/errors/view-403.js
@@ -0,0 +1,19 @@
+/**
+ * @file controllers/errors/view-403.js
+ * @project best-trip
+ * @version 0.0.0
+ * @author best-trip
+ * @date 03 June, 2024
+ * @update_date 03 June, 2024
+ */
+
+// export 403 error view controller
+module.exports = (req, res) => {
+    try {
+        res.render('errors/403', {
+            title: '403 - Forbidden',
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
