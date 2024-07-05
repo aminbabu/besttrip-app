985b2d47a1f7e2d58ebb925855c6a60a60f9ab75
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 08:42:27 +0600

    Update error routes in dashboard

--- routes/dashboard/errors/index.js
+++ routes/dashboard/errors/index.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 03 June, 2024
- * @update_date 03 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
@@ -14,11 +14,16 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { view401, view404, view500 } = require('../../../controllers/dashboard/errors');
+const {
+    view401,
+    view403,
+    view404,
+    view500,
+} = require('../../../controllers/dashboard/errors');
 
 /**
  * @description view 401 error
- * @param {string} path - //dashboard/auth/login
+ * @param {string} path - /dashboard/errors/401
  * @param {function} controller - ['view401']
  * @returns {object} - router
  * @access public
@@ -27,8 +32,18 @@ const { view401, view404, view500 } = require('../../../controllers/dashboard/er
 router.get('/401', view401);
 
 /**
+ * @description view 403 error
+ * @param {string} path - /dashboard/errors/403
+ * @param {function} controller - ['view403']
+ * @returns {object} - router
+ * @access public
+ * @method GET
+ */
+router.get('/403', view403);
+
+/**
  * @description view 404 error
- * @param {string} path - /errors/404
+ * @param {string} path - /dashboard/errors/404
  * @param {function} controller - ['view404']
  * @returns {object} - router
  * @access public
@@ -38,7 +53,7 @@ router.get('/404', view404);
 
 /**
  * @description view 500 error
- * @param {string} path - /errors/500
+ * @param {string} path - /dashboard/errors/500
  * @param {function} controller - ['view500']
  * @returns {object} - router
  * @access public
