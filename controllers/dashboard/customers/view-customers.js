48fe3a323ebc2ab6cff739551d55e983b1b223bf
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 11:23:39 +0600

    Refactor customers view controller and add currency formatting

--- controllers/dashboard/customers/view-customers.js
+++ controllers/dashboard/customers/view-customers.js
@@ -4,41 +4,45 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 June, 2024
- * @update_date 23 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
-const { countries } = require("countries-list");
-const { Customer } = require("../../../models");
-const moment = require("moment");
+const { countries } = require('countries-list');
+const { Customer } = require('../../../models');
+const moment = require('moment');
+const { currencyFormatter } = require('../../../utils/global');
 
 // export customers view controller
 module.exports = async (req, res) => {
-  try {
-    // get customers
-    let customers = await Customer.find()
-      .populate("wallet")
-      .sort({ createdAt: -1 });
+    try {
+        // get customers
+        let customers = await Customer.find()
+            .populate('wallet')
+            .sort({ createdAt: -1 });
 
-    // formate data
-    customers = customers.map((customer) => {
-      const customerObj = { ...customer.toObject() };
+        // formate data and balance
+        customers = customers.map((customer) => {
+            const customerObj = { ...customer.toObject() };
 
-      customerObj.createdAt = moment(customer.createdAt).format(
-        "DD MMM YYYY, h:mm a"
-      );
+            customerObj.createdAt = moment(customer.createdAt).format(
+                'DD MMM YYYY, h:mm a'
+            );
+            customerObj.wallet.balance = currencyFormatter(
+                customer.wallet.balance
+            );
 
-      return customerObj;
-    });
+            return customerObj;
+        });
 
-    // return render view
-    return res.render("dashboard/customers", {
-      title: "Customers",
-      user: req.user,
-      customers,
-      countries: Object.values(countries),
-    });
-  } catch (error) {
-    return res.redirect("/error/500");
-  }
+        // return render view
+        return res.render('dashboard/customers', {
+            title: 'Customers',
+            user: req.user,
+            customers,
+            countries: Object.values(countries),
+        });
+    } catch (error) {
+        return res.redirect('/dashboard/error/500');
+    }
 };
