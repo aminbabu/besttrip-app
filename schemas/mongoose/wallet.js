99c4d0924eed12025d1742dc623a85f6401a4a04
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 17:54:53 +0600

    Refactor wallet schema

--- schemas/mongoose/wallet.js
+++ schemas/mongoose/wallet.js
@@ -4,43 +4,37 @@
  * @version 0.0.0
  * @author best-trip
  * @date 24 June, 2024
- * @update_date 24 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
-const { Schema } = require("mongoose");
-const { WALLET_TRANSACTION_TYPES } = require("../../constants");
+const { Schema } = require('mongoose');
+const { WALLET_TRANSACTION_TYPES } = require('../../constants');
 
 // export customer schema
 module.exports = new Schema(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: [() => this.user, "User is required"],
-      unique: [true, "User already has a wallet"],
+    {
+        customer: {
+            type: Schema.Types.ObjectId,
+            ref: 'Customer',
+            required: [true, 'Customer is required'],
+            unique: [true, 'Customer already has a wallet'],
+        },
+        balance: {
+            type: Number,
+            required: [true, 'Wallet balance is required'],
+            default: 0,
+        },
+        type: {
+            type: String,
+            enum: WALLET_TRANSACTION_TYPES,
+            default: WALLET_TRANSACTION_TYPES[0],
+        },
+        description: {
+            type: String,
+        },
     },
-    customer: {
-      type: Schema.Types.ObjectId,
-      ref: "Customer",
-      required: [() => this.customer, "Customer is required"],
-      unique: [true, "Customer already has a wallet"],
-    },
-    balance: {
-      type: Number,
-      required: [true, "Wallet balance is required"],
-      default: 0,
-    },
-    type: {
-      type: String,
-      enum: WALLET_TRANSACTION_TYPES,
-      default: WALLET_TRANSACTION_TYPES[0],
-    },
-    description: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
