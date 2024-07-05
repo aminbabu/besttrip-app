d6aa3673fbc2a7aa1cb8ba922a146df7ca56d7ed
Amin Babu <amin.babu.bd@gmail.com>
2024-07-05 10:48:56 +0600

    Update customer view controller with additional dependencies and formatting

--- controllers/dashboard/customers/view-customer.js
+++ controllers/dashboard/customers/view-customer.js
@@ -4,11 +4,13 @@
  * @version 0.0.0
  * @author best-trip
  * @date 14 June, 2024
- * @update_date 15 June, 2024
+ * @update_date 05 Jul, 2024
  */
 
 // dependencies
+const moment = require('moment');
 const { Customer } = require('../../../models');
+const { countries } = require('countries-list');
 
 // export customer view controller
 module.exports = async (req, res) => {
@@ -16,18 +18,40 @@ module.exports = async (req, res) => {
         // get customer id from request params
         const { id } = req.params;
 
-        // get customer
-        const customer = await Customer.findById(id);
+        // get customer by id with wallet, bookings, and payment-requests in descending order
+        const existingcustomer = await Customer.findById(id)
+            .populate({
+                path: 'wallet',
+            })
+            .populate({
+                path: 'umrahBookings',
+                options: { sort: { createdAt: -1 } },
+            })
+            .populate({
+                path: 'paymentRequests',
+                options: { sort: { createdAt: -1 } },
+            });
 
         // check if customer exists
-        if (!customer) {
+        if (!existingcustomer) {
             return res.redirect('/dashboard/error/404');
         }
 
+        const customer = existingcustomer.toObject();
+
+        // format customer dates
+        customer.createdAt = moment(customer.createdAt).format(
+            'DD MMM YYYY, h:mm a'
+        );
+        customer.updatedAt = moment(customer.updatedAt).format(
+            'DD MMM YYYY, h:mm a'
+        );
+
         // return rendered view
         return res.render('dashboard/customers/customer', {
-            title: 'Edit Customer',
+            title: customer.name,
             customer,
+            countries: Object.values(countries),
         });
     } catch (error) {
         return res.redirect('/dashboard/error/500');
