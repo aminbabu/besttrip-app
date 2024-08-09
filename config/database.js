5f374b73779f8cef6836c79cf89fa89da57cf9e3
JoySarkarBD <developer.joysarkar@gmail.com>
2024-08-10 03:18:33 +0600

    config: new config added

--- config/database.js
+++ config/database.js
@@ -9,12 +9,12 @@
 
 // dependencies
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('./env');
+const { MONGODB_URI, MONGODB_LIVE_URI } = require('./env');
 
 // connect to database
 const connect = async () => {
     try {
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(/* MONGODB_LIVE_URI || */ MONGODB_URI);
         console.log(`Database connected in ${process.env.NODE_ENV} mode`);
     } catch (error) {
         console.error('Database connection failed');
