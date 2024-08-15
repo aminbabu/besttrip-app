0ae678afb2b115880942a4fd8cc06df727694695
JoySarkarBD <developer.joysarkar@gmail.com>
2024-08-15 17:48:38 +0600

    fix: Properly format BDT currency, including handling of negative values

--- utils/global/currency-formatter.js
+++ utils/global/currency-formatter.js
@@ -4,7 +4,7 @@
  * @version 0.0.0
  * @author best-trip
  * @date 16 June, 2024
- * @update_date 16 June, 2024
+ * @update_date 15 Aug, 2024
  */
 
 // dependencies
@@ -18,4 +18,5 @@ module.exports = (price) =>
         decimal: '.',
         precision: 2,
         pattern: '# !',
+        negativePattern: '-# !',
     }).format();
