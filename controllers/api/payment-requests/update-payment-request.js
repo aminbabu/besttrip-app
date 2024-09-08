d68c25cc98109d7101252745b15f5c267c31feb7
JoySarkarBD <developer.joysarkar@gmail.com>
2024-09-08 21:00:36 +0600

    payment request updated

--- controllers/api/payment-requests/update-payment-request.js
+++ controllers/api/payment-requests/update-payment-request.js
@@ -8,7 +8,7 @@
  */
 
 // dependencies
-const { PaymentRequest, Customer } = require('../../../models');
+const { PaymentRequest, Wallet } = require('../../../models');
 const mongoose = require('mongoose');
 
 // export update payment request controller
@@ -31,23 +31,53 @@ module.exports = async (req, res, next) => {
             },
             { $unwind: '$customer' },
             {
+                $lookup: {
+                    from: 'wallets',
+                    localField: 'customer.wallet',
+                    foreignField: '_id',
+                    as: 'wallet',
+                },
+            },
+            { $unwind: '$wallet' },
+            {
                 $project: {
+                    _id: 0,
+                    __v: 0,
+                    paymentMethod: 0,
                     'customer.password': 0,
                     'customer.twoStepAuth': 0,
                     'customer.isVerified': 0,
                     'customer.loginHistory': 0,
                     'customer.createdAt': 0,
                     'customer.updatedAt': 0,
+                    'customer.customer': 0,
+                    'customer.status': 0,
+                    'customer.avatar': 0,
+                    'customer.name': 0,
+                    'customer.email': 0,
+                    'customer.phone': 0,
+                    'customer.role': 0,
+                    'customer.dob': 0,
+                    'customer.__v': 0,
+                    refId: 0,
+                    attachment: 0,
+                    paymentDate: 0,
+                    account: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    'wallet.__v': 0,
+                    'wallet.createdAt': 0,
+                    'wallet.updatedAt': 0,
                 },
             },
         ]);
 
         const paymentRequest = paymentRequests[0];
 
-        // check if payment request exist
+        // check if payment request exists
         if (!paymentRequest) {
             return res.status(404).json({
-                message: 'Payments request not found',
+                message: 'Payment request not found',
             });
         }
 
@@ -58,21 +88,19 @@ module.exports = async (req, res, next) => {
             });
         }
 
-        // get the customer
-        const customer = await Customer.findById(paymentRequest.customer._id);
+        // get the customer wallet
+        const customerWallet = await Wallet.findById(paymentRequest.wallet._id);
 
         // check if payment request is approved
         if (paymentRequest.status !== 'approved' && status === 'approved') {
             // update customer balance
-            paymentRequest.customer.wallet.balance += paymentRequest.amount;
-            customer.wallet.balance += paymentRequest.amount;
+            customerWallet.balance += paymentRequest.amount;
         }
 
         // check if payment request is rejected
         if (paymentRequest.status === 'approved' && status === 'rejected') {
             // update customer balance
-            paymentRequest.customer.wallet.balance -= paymentRequest.amount;
-            customer.wallet.balance -= paymentRequest.amount;
+            customerWallet.balance -= paymentRequest.amount;
         }
 
         // update payment request
@@ -82,8 +110,8 @@ module.exports = async (req, res, next) => {
         // save payment request
         await PaymentRequest.findByIdAndUpdate(id, paymentRequest);
 
-        // save customer
-        await customer.save();
+        // save customer wallet
+        await customerWallet.save();
 
         // return response
         return res.status(200).json({
